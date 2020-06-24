bb0f336050133cd5877addc7304b19c1c76a2c23
Kautenja <ckfour@icloud.com>
2020-06-24 02:59:15 -0500

    fix headers

--- src/dsp/blip_buffer/blip_buffer.hpp
+++ src/dsp/blip_buffer/blip_buffer.hpp
@@ -1,8 +1,17 @@
-
-// Buffer of sound samples into which band-limited waveforms can be synthesized
-// using Blip_Wave or Blip_Synth.
-
-// Blip_Buffer 0.3.3. Copyright (C) 2003-2005 Shay Green. GNU LGPL license.
+// Band-limited waveform buffer.
+// Copyright 2020 Christian Kauten
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef BLIP_BUFFER_BLIP_BUFFER_HPP
 #define BLIP_BUFFER_BLIP_BUFFER_HPP

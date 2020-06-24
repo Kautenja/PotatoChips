df4223c4722bd66865e8558b26ec3bfeaeaba062
Kautenja <ckfour@icloud.com>
2020-06-24 00:12:44 -0500

    remove cstdint int

--- src/dsp/blip_buffer/blip_buffer.cpp
+++ src/dsp/blip_buffer/blip_buffer.cpp
@@ -14,6 +14,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
 #include "blip_buffer.h"
 #include <cstring>
 #include <cmath>
+#include <climits>
 #include "blargg_source.h"
 
 Blip_Buffer::Blip_Buffer() {

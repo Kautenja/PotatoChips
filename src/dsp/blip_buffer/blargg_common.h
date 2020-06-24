823b7bdfc15c9fd82769aa7eb2eca412ed231daa
Kautenja <ckfour@icloud.com>
2020-06-24 00:37:44 -0500

    remove static_assert files

--- src/dsp/blip_buffer/blargg_common.h
+++ src/dsp/blip_buffer/blargg_common.h
@@ -99,7 +99,24 @@
 #endif
 
 // BOOST_STATIC_ASSERT( expr )
-#include "boost/static_assert.hpp"
+#ifndef BOOST_STATIC_ASSERT_HPP
+#define BOOST_STATIC_ASSERT_HPP
+
+#if defined (_MSC_VER) && _MSC_VER <= 1200
+    // MSVC6 can't handle the ##line concatenation
+    #define BOOST_STATIC_ASSERT( expr ) struct { int n [1 / ((expr) ? 1 : 0)]; }
+
+#else
+    #define BOOST_STATIC_ASSERT3( expr, line ) \
+                typedef int boost_static_assert_##line [1 / ((expr) ? 1 : 0)]
+
+    #define BOOST_STATIC_ASSERT2( expr, line ) BOOST_STATIC_ASSERT3( expr, line )
+
+    #define BOOST_STATIC_ASSERT( expr ) BOOST_STATIC_ASSERT2( expr, __LINE__ )
+
+#endif
+
+#endif
 
 // Common standard headers
 #if BLARGG_COMPILER_HAS_NAMESPACE

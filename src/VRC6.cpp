56149ab3451c2f35f50dfde6b7d05800ef18d6b3
Kautenja <ckfour@icloud.com>
2020-06-23 23:59:10 -0500

    reorganize DSP files

--- src/VRC6.cpp
+++ src/VRC6.cpp
@@ -17,7 +17,7 @@
 
 #include "plugin.hpp"
 #include "components.hpp"
-#include "dsp/Nes_Vrc6.h"
+#include "dsp/vrc6.h"
 
 /// the IO registers on the VRC6 chip (altered for VRC6 implementation).
 enum IORegisters {

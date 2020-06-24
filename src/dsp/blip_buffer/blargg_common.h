9be126ef6d8c3bf829b86c287ae47c4b087699ea
Kautenja <ckfour@icloud.com>
2020-06-24 01:18:07 -0500

    remove BLARGG_STATIC_ASSERT

--- src/dsp/blip_buffer/blargg_common.h
+++ src/dsp/blip_buffer/blargg_common.h
@@ -9,26 +9,6 @@
 #ifndef BLARGG_COMMON_H
 #define BLARGG_COMMON_H
 
-// BOOST_STATIC_ASSERT( expr )
-#ifndef BOOST_STATIC_ASSERT_HPP
-#define BOOST_STATIC_ASSERT_HPP
-
-#if defined (_MSC_VER) && _MSC_VER <= 1200
-    // MSVC6 can't handle the ##line concatenation
-    #define BOOST_STATIC_ASSERT( expr ) struct { int n [1 / ((expr) ? 1 : 0)]; }
-
-#else
-    #define BOOST_STATIC_ASSERT3( expr, line ) \
-                typedef int boost_static_assert_##line [1 / ((expr) ? 1 : 0)]
-
-    #define BOOST_STATIC_ASSERT2( expr, line ) BOOST_STATIC_ASSERT3( expr, line )
-
-    #define BOOST_STATIC_ASSERT( expr ) BOOST_STATIC_ASSERT2( expr, __LINE__ )
-
-#endif
-
-#endif
-
 #include <cstddef>
 #include <cassert>
 #include <new>

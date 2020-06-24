4befc6ebee3611b5a93e24746d192efd61568e18
Kautenja <ckfour@icloud.com>
2020-06-24 01:04:37 -0500

    cleanup C++ style

--- src/dsp/blip_buffer/blip_buffer.h
+++ src/dsp/blip_buffer/blip_buffer.h
@@ -10,29 +10,31 @@
 #include "blargg_common.h"
 #include <cstdint>
 
+/// forward declaration of the Blip_Reader class
 class Blip_Reader;
 
-// Source time unit.
+/// Source time unit.
 typedef int32_t blip_time_t;
 
-// Type of sample produced. Signed 16-bit format.
+/// Type of sample produced. Signed 16-bit format.
 typedef int16_t blip_sample_t;
 
-// Make buffer as large as possible (currently about 65000 samples)
+/// Make buffer as large as possible (currently about 65000 samples)
 const int blip_default_length = 0;
 
 class Blip_Buffer {
-public:
+ public:
     // Construct an empty buffer.
     Blip_Buffer();
     ~Blip_Buffer();
 
-    // Set output sample rate and buffer length in milliseconds (1/1000 sec),
-    // then clear buffer. If length is not specified, make as large as possible.
-    // If there is insufficient memory for the buffer, sets the buffer length
-    // to 0 and returns error string (or propagates exception if compiler supports it).
+    /// TODO: rename to set_sample_rate
+    /// Set output sample rate and buffer length in milliseconds (1/1000 sec),
+    /// then clear buffer. If length is not specified, make as large as
+    /// possible. If there is insufficient memory for the buffer, sets the
+    /// buffer length to 0 and returns error string or propagates exception
+    /// if compiler supports it.
     blargg_err_t sample_rate(int32_t samples_per_sec, int msec_length = blip_default_length);
-    // to do: rename to set_sample_rate
 
     // Length of buffer, in milliseconds
     int length() const;
@@ -42,49 +44,47 @@ public:
 
     // Number of source time units per second
     void clock_rate(int32_t);
+
     int32_t clock_rate() const;
 
     // Set frequency at which high-pass filter attenuation passes -3dB
     void bass_freq(int frequency);
 
-    // Remove all available samples and clear buffer to silence. If 'entire_buffer' is
-    // false, just clear out any samples waiting rather than the entire buffer.
+    /// Remove all available samples and clear buffer to silence. If
+    /// 'entire_buffer' is false, just clear out any samples waiting rather
+    /// than the entire buffer.
     void clear(bool entire_buffer = true);
 
-    // to do:
-    // Notify Blip_Buffer that synthesis has been performed until specified time
-    //void run_until(blip_time_t);
-
-    // End current time frame of specified duration and make its samples available
-    // (aint32_t with any still-unread samples) for reading with read_samples(). Begin
-    // a new time frame at the end of the current frame. All transitions must have
-    // been added before 'time'.
+    /// End current time frame of specified duration and make its samples
+    /// available (aint32_t with any still-unread samples) for reading with
+    /// read_samples(). Begin a new time frame at the end of the current
+    /// frame. All transitions must have been added before 'time'.
     void end_frame(blip_time_t time);
 
     // Number of samples available for reading with read_samples()
     int32_t samples_avail() const;
 
-    // Read at most 'max_samples' out of buffer into 'dest', removing them from from
-    // the buffer. Return number of samples actually read and removed. If stereo is
-    // true, increment 'dest' one extra time after writing each sample, to allow
-    // easy interleving of two channels into a stereo output buffer.
+    /// Read at most 'max_samples' out of buffer into 'dest', removing them
+    /// from the buffer. Return number of samples actually read and removed.
+    /// If stereo is true, increment 'dest' one extra time after writing each
+    /// sample, to allow easy interleving of two channels into a stereo output
+    /// buffer.
     int32_t read_samples(blip_sample_t* dest, int32_t max_samples, bool stereo = false);
 
-    // Remove 'count' samples from those waiting to be read
+    /// Remove 'count' samples from those waiting to be read
     void remove_samples(int32_t count);
 
-    // Number of samples delay from synthesis to samples read out
+    /// Number of samples delay from synthesis to samples read out
     int output_latency() const;
-
 
     // Experimental external buffer mixing support
 
-    // Number of raw samples that can be mixed within frame of specified duration
+    /// Number of raw samples that can be mixed within frame of specified
+    /// duration
     int32_t count_samples(blip_time_t duration) const;
 
-    // Mix 'count' samples from 'buf' into buffer.
+    /// Mix 'count' samples from 'buf' into buffer.
     void mix_samples(const blip_sample_t* buf, int32_t count);
-
 
     // not documented yet
 
@@ -100,13 +100,13 @@ public:
         return t * resampled_time_t (factor_);
     }
 
-private:
+ private:
     // noncopyable
     Blip_Buffer(const Blip_Buffer&);
     Blip_Buffer& operator = (const Blip_Buffer&);
 
-    // Don't use the following members. They are public only for technical reasons.
-    public:
+// Don't use the following members. They are public only for technical reasons.
+ public:
         enum { widest_impulse_ = 24 };
         typedef uint16_t buf_t_;
 
@@ -114,7 +114,8 @@ private:
         resampled_time_t offset_;
         buf_t_* buffer_;
         unsigned buffer_size_;
-    private:
+
+ private:
         int32_t reader_accum;
         int bass_shift;
         int32_t samples_per_sec;
@@ -130,10 +131,11 @@ private:
 
 // Low-pass equalization parameters (see notes.txt)
 class blip_eq_t {
-public:
+ public:
     blip_eq_t(double treble = 0);
     blip_eq_t(double treble, int32_t cutoff, int32_t sample_rate);
-private:
+
+ private:
     double treble;
     int32_t cutoff;
     int32_t sample_rate;
@@ -145,9 +147,10 @@ class Blip_Reader {
     const Blip_Buffer::buf_t_* buf;
     int32_t accum;
     #ifdef __MWERKS__
-    void operator = (struct foobar); // helps optimizer
+    void operator=(struct foobar);  // helps optimizer
     #endif
-public:
+
+ public:
     // avoid anything which might cause optimizer to put object in memory
 
     int begin(Blip_Buffer& blip_buf) {
@@ -194,7 +197,8 @@ class Blip_Impulse_ {
 
     void fine_volume_unit();
     void scale_impulse(int unit, imp_t*) const;
-public:
+
+ public:
     Blip_Buffer*    buf;
     uint32_t offset;
 

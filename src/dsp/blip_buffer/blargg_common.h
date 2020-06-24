9a751cd61d7436e4ba6025b6c71c459cc75687fb
Kautenja <ckfour@icloud.com>
2020-06-24 01:20:11 -0500

    remove useless definitions

--- src/dsp/blip_buffer/blargg_common.h
+++ src/dsp/blip_buffer/blargg_common.h
@@ -17,35 +17,4 @@
 typedef const char* blargg_err_t;
 const blargg_err_t blargg_success = 0;
 
-// BLARGG_BIG_ENDIAN and BLARGG_LITTLE_ENDIAN
-// Only needed if modules are used which must know byte order.
-#if !defined (BLARGG_BIG_ENDIAN) && !defined (BLARGG_LITTLE_ENDIAN)
-    #if defined (__powerc) || defined (macintosh)
-        #define BLARGG_BIG_ENDIAN 1
-
-    #elif defined (_MSC_VER) && defined (_M_IX86)
-        #define BLARGG_LITTLE_ENDIAN 1
-
-    #endif
-#endif
-
-// BLARGG_NONPORTABLE (allow use of nonportable optimizations/features)
-#ifndef BLARGG_NONPORTABLE
-    #define BLARGG_NONPORTABLE 0
-#endif
-#ifdef BLARGG_MOST_PORTABLE
-    #error "BLARGG_MOST_PORTABLE has been removed; use BLARGG_NONPORTABLE."
-#endif
-
-// BLARGG_CPU_*
-#if !defined (BLARGG_CPU_POWERPC) && !defined (BLARGG_CPU_X86)
-    #if defined (__powerc)
-        #define BLARGG_CPU_POWERPC 1
-
-    #elif defined (_MSC_VER) && defined (_M_IX86)
-        #define BLARGG_CPU_X86 1
-
-    #endif
-#endif
-
 #endif  // BLARGG_COMMON_H

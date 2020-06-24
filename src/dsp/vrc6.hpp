a7cace7ec9bd9b88f77def2a3d4c6e94b3fa7dd8
Kautenja <ckfour@icloud.com>
2020-06-24 03:53:28 -0500

    document

--- src/dsp/vrc6.hpp
+++ src/dsp/vrc6.hpp
@@ -21,19 +21,20 @@
 /// A macro oscillator based on the Konami VRC6 synthesis chip.
 class VRC6 {
  public:
-    VRC6() {
-        output(NULL);
-        volume(1.0);
-        reset();
-    }
+    /// the number of oscillators on the VRC6 chip
+    static constexpr int OSC_COUNT = 3;
+    /// the number of registers per oscillator
+    static constexpr int REG_COUNT = 3;
 
-    // See APU.h for reference
-    void reset() {
+    /// Initialize a new VRC6 chip emulator.
+    VRC6() { output(NULL); volume(1.0); reset(); }
+
+    /// Reset internal frame counter, registers, and all oscillators.
+    inline void reset() {
         last_time = 0;
         for (int i = 0; i < OSC_COUNT; i++) {
-            Vrc6_Osc& osc = oscs[i];
-            for (int j = 0; j < reg_count; j++)
-                osc.regs[j] = 0;
+            VRC6_Oscillator& osc = oscs[i];
+            for (int j = 0; j < REG_COUNT; j++) osc.regs[j] = 0;
             osc.delay = 0;
             osc.last_amp = 0;
             osc.phase = 1;
@@ -41,71 +42,111 @@ class VRC6 {
         }
     }
 
-    void volume(double v) {
-        v *= 0.0967 * 2;
-        saw_synth.volume(v);
-        square_synth.volume(v * 0.5);
+    /// Set the volume.
+    ///
+    /// @param value the global volume level of the chip
+    ///
+    inline void volume(double value) {
+        value *= 0.0967 * 2;
+        saw_synth.volume(value);
+        square_synth.volume(value * 0.5);
     }
 
-    void treble_eq(blip_eq_t const& eq) {
-        saw_synth.treble_eq(eq);
-        square_synth.treble_eq(eq);
+    /// Set treble equalization.
+    ///
+    /// @param eq the equalizer settings to use
+    ///
+    inline void treble_eq(blip_eq_t const& equalizer) {
+        saw_synth.treble_eq(equalizer);
+        square_synth.treble_eq(equalizer);
     }
 
-    void output(BLIPBuffer* buf) {
+    /// Set buffer to generate all sound into, or disable sound if NULL.
+    ///
+    /// @param buf the buffer to write samples from the synthesizer to
+    ///
+    inline void output(BLIPBuffer* buf) {
         for (int i = 0; i < OSC_COUNT; i++) osc_output(i, buf);
     }
 
-    static constexpr int OSC_COUNT = 3;
+    /// Set the output buffer for an individual synthesizer voice.
+    ///
+    /// @param i the index of the oscillator to set the output buffer for
+    /// @param buf the buffer to write samples from the synthesizer to
+    ///
     inline void osc_output(int i, BLIPBuffer* buf) {
-        // assert((unsigned) i < OSC_COUNT);
+        assert((unsigned) i < OSC_COUNT);
         oscs[i].output = buf;
     }
 
-    void end_frame(cpu_time_t time) {
+    /// Run all oscillators up to specified time, end current time frame, then
+    /// start a new time frame at time 0. Time frames have no effect on
+    /// emulation and each can be whatever length is convenient.
+    ///
+    /// @param time the number of elapsed cycles
+    ///
+    inline void end_frame(cpu_time_t time) {
         if (time > last_time) run_until(time);
         last_time -= time;
         assert(last_time >= 0);
     }
 
-    // Oscillator 0 write-only registers are at $9000-$9002
-    // Oscillator 1 write-only registers are at $A000-$A002
-    // Oscillator 2 write-only registers are at $B000-$B002
-    enum { reg_count = 3 };
-    enum { base_addr = 0x9000 };
-    enum { addr_step = 0x1000 };
-    void write_osc(cpu_time_t time, int osc_index, int reg, int data) {
+    /// Write a value to the given oscillator's register.
+    ///
+    /// @param time the number of elapsed cycles
+    /// @param osc_index the index of the oscillator
+    /// @param reg the index of the synthesizer's register
+    /// @param data the data to write to the register value
+    ///
+    inline void write_osc(cpu_time_t time, int osc_index, int reg, int data) {
         assert((unsigned) osc_index < OSC_COUNT);
-        assert((unsigned) reg < reg_count);
-
+        assert((unsigned) reg < REG_COUNT);
         run_until(time);
         oscs[osc_index].regs[reg] = data;
     }
 
  private:
-    // noncopyable
+    /// Disable the public copy constructor.
     VRC6(const VRC6&);
+
+    /// Disable the public assignment operator.
     VRC6& operator = (const VRC6&);
 
-    struct Vrc6_Osc {
+    /// An oscillator on the VRC6 chip.
+    struct VRC6_Oscillator {
+        /// the internal registers for the oscillator
         uint8_t regs[3];
+        /// the output buffer to write samples to
         BLIPBuffer* output;
+        /// TODO: document
         int delay;
+        /// TODO: document
         int last_amp;
+        /// the phase of the waveform
         int phase;
-        int amp;  // only used by saw
+        /// the amplitude of the waveform, only used by the saw waveform
+        int amp;
 
-        int period() const {
+        /// Return the period of the waveform.
+        inline int period() const {
             return (regs[2] & 0x0f) * 0x100L + regs[1] + 1;
         }
     };
 
-    Vrc6_Osc oscs[OSC_COUNT];
+    /// the oscillators on the chip
+    VRC6_Oscillator oscs[OSC_COUNT];
+    /// TODO: document
     cpu_time_t last_time;
 
+    /// a BLIP synthesizer for the saw waveform
     BLIPSynth<BLIPQuality::Medium, 31> saw_synth;
+    /// a BLIP synthesizer for the square waveform
     BLIPSynth<BLIPQuality::Good, 15> square_synth;
 
+    /// Run VRC6 until specified time.
+    ///
+    /// @param time the number of elapsed cycles
+    ///
     void run_until(cpu_time_t time) {
         assert(time >= last_time);
         run_square(oscs[0], time);
@@ -114,13 +155,17 @@ class VRC6 {
         last_time = time;
     }
 
-    void run_square(Vrc6_Osc& osc, cpu_time_t end_time) {
+    /// Run a square waveform until specified time.
+    ///
+    /// @param osc the oscillator to run
+    /// @param time the number of elapsed cycles
+    ///
+    void run_square(VRC6_Oscillator& osc, cpu_time_t end_time) {
         BLIPBuffer* output = osc.output;
         if (!output) return;
 
         int volume = osc.regs[0] & 15;
-        if (!(osc.regs[2] & 0x80))
-            volume = 0;
+        if (!(osc.regs[2] & 0x80)) volume = 0;
 
         int gate = osc.regs[0] & 0x80;
         int duty = ((osc.regs[0] >> 4) & 7) + 1;
@@ -137,7 +182,6 @@ class VRC6 {
         if (volume && !gate && period > 4) {
             if (time < end_time) {
                 int phase = osc.phase;
-
                 do {
                     phase++;
                     if (phase == 16) {
@@ -151,23 +195,26 @@ class VRC6 {
                     }
                     time += period;
                 } while (time < end_time);
-
                 osc.phase = phase;
             }
             osc.delay = time - end_time;
         }
     }
 
+    /// Run a saw waveform until specified time.
+    ///
+    /// @param time the number of elapsed cycles
+    ///
     void run_saw(cpu_time_t end_time) {
-        Vrc6_Osc& osc = oscs[2];
+        VRC6_Oscillator& osc = oscs[2];
         BLIPBuffer* output = osc.output;
-        if (!output)
-            return;
+        if (!output) return;
 
         int amp = osc.amp;
         int amp_step = osc.regs[0] & 0x3F;
         cpu_time_t time = last_time;
         int last_amp = osc.last_amp;
+
         if (!(osc.regs[2] & 0x80) || !(amp_step | amp)) {
             osc.delay = 0;
             int delta = (amp >> 3) - last_amp;
@@ -178,30 +225,24 @@ class VRC6 {
             if (time < end_time) {
                 int period = osc.period() * 2;
                 int phase = osc.phase;
-
                 do {
                     if (--phase == 0) {
                         phase = 7;
                         amp = 0;
                     }
-
                     int delta = (amp >> 3) - last_amp;
                     if (delta) {
                         last_amp = amp >> 3;
                         saw_synth.offset(time, delta, output);
                     }
-
                     time += period;
                     amp = (amp + amp_step) & 0xFF;
                 } while (time < end_time);
-
                 osc.phase = phase;
                 osc.amp = amp;
             }
-
             osc.delay = time - end_time;
         }
-
         osc.last_amp = last_amp;
     }
 };

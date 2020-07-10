b47449dc16053eeb5caefb4829e665b2f27028bb
Kautenja <kautencreations@gmail.com>
2020-07-10 02:27:09 -0500

    copyright

--- src/dsp/Gb_Apu.h
+++ src/dsp/Gb_Apu.h
@@ -1,6 +1,21 @@
 // Nintendo Game Boy PAPU sound chip emulator
+// Copyright 2020 Christian Kauten
+// Copyright 2006 Shay Green
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// derived from: Game_Music_Emu 0.5.2
+//
 
-// Gb_Snd_Emu 0.1.5
 #ifndef GB_APU_H
 #define GB_APU_H
 

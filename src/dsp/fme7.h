c42a571fb5e6edc9a2c9dc92cb64fe3d54642144
Kautenja <kautencreations@gmail.com>
2020-07-09 19:17:20 -0500

    remove CPP file

--- src/dsp/fme7.h
+++ src/dsp/fme7.h
@@ -1,12 +1,40 @@
-// Sunsoft FME-7 sound emulator
+// A macro oscillator based on the Sunsoft 5B synthesis chip.
+// Copyright 2020 Christian Kauten
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// derived from: Game_Music_Emu 0.5.2
+//
 
-// Game_Music_Emu 0.5.2
-#ifndef NES_FME7_APU_H
-#define NES_FME7_APU_H
+#ifndef NES_FME7_APU_HPP_
+#define NES_FME7_APU_HPP_
 
 #include "blip_buffer/blip_buffer.hpp"
 #include "blip_buffer/blip_synth.hpp"
 #include <cstdint>
+
+// can be any value; this gives best error/quality tradeoff
+enum { amp_range = 192 };
+
+// static unsigned char const amp_table [16];
+static constexpr unsigned char amp_table[16] =
+{
+	#define ENTRY( n ) (unsigned char) (n * amp_range + 0.5)
+	ENTRY(0.0000), ENTRY(0.0078), ENTRY(0.0110), ENTRY(0.0156),
+	ENTRY(0.0221), ENTRY(0.0312), ENTRY(0.0441), ENTRY(0.0624),
+	ENTRY(0.0883), ENTRY(0.1249), ENTRY(0.1766), ENTRY(0.2498),
+	ENTRY(0.3534), ENTRY(0.4998), ENTRY(0.7070), ENTRY(1.0000)
+	#undef ENTRY
+};
 
 class FME7 {
  private:
@@ -18,7 +46,11 @@ class FME7 {
 
  public:
 	// See Nes_Apu.h for reference
-	void reset();
+	void reset() {
+		last_time = 0;
+		for (int i = 0; i < OSC_COUNT; i++) oscs[i].last_amp = 0;
+	}
+
 	void volume( double );
 	void treble_eq( blip_eq_t const& );
 	void output( BLIPBuffer* );
@@ -45,18 +77,82 @@ class FME7 {
 	FME7( const FME7& );
 	FME7& operator = ( const FME7& );
 
-	static unsigned char const amp_table [16];
-
 	struct {
 		BLIPBuffer* output;
 		int last_amp;
 	} oscs [OSC_COUNT];
 	blip_time_t last_time;
 
-	enum { amp_range = 192 }; // can be any value; this gives best error/quality tradeoff
 	BLIPSynth<BLIPQuality::Good, 1> synth;
 
-	void run_until( blip_time_t );
+	void run_until(blip_time_t end_time) {
+		// require( end_time >= last_time );
+
+		for ( int index = 0; index < OSC_COUNT; index++ )
+		{
+			// int mode = regs [7] >> index;
+			int vol_mode = regs [010 + index];
+			int volume = amp_table [vol_mode & 0x0F];
+
+			BLIPBuffer* const osc_output = oscs [index].output;
+			if ( !osc_output )
+				continue;
+
+			// period
+			int const period_factor = 16;
+			unsigned period = (regs [index * 2 + 1] & 0x0F) * 0x100 * period_factor +
+					regs [index * 2] * period_factor;
+			if ( period < 50 ) // around 22 kHz
+			{
+				volume = 0;
+				if ( !period ) // on my AY-3-8910A, period doesn't have extra one added
+					period = period_factor;
+			}
+
+			// current amplitude
+			int amp = volume;
+			if ( !phases [index] )
+				amp = 0;
+			{
+				int delta = amp - oscs [index].last_amp;
+				if ( delta )
+				{
+					oscs [index].last_amp = amp;
+					synth.offset( last_time, delta, osc_output );
+				}
+			}
+
+			blip_time_t time = last_time + delays [index];
+			if ( time < end_time )
+			{
+				int delta = amp * 2 - volume;
+				if ( volume )
+				{
+					do
+					{
+						delta = -delta;
+						synth.offset_inline( time, delta, osc_output );
+						time += period;
+					}
+					while ( time < end_time );
+
+					oscs [index].last_amp = (delta + volume) >> 1;
+					phases [index] = (delta > 0);
+				}
+				else
+				{
+					// maintain phase when silent
+					int count = (end_time - time + period - 1) / period;
+					phases [index] ^= count & 1;
+					time += (long) count * period;
+				}
+			}
+
+			delays [index] = time - end_time;
+		}
+
+		last_time = end_time;
+	}
 };
 
 inline void FME7::volume( double v )

0e7eab20c3eeb304003ef62c902c633191d6fba0
Kautenja <kautencreations@gmail.com>
2020-07-10 02:30:00 -0500

    Sega Master System SN76489 PSG

---
+++ src/dsp/Sms_Oscs.h
@@ -0,0 +1,63 @@
+// Private oscillators used by Sms_Apu
+// Copyright 2020 Christian Kauten
+// Copyright 2006 Shay Green
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// derived from: Game_Music_Emu 0.5.2
+//
+
+#ifndef SMS_OSCS_H
+#define SMS_OSCS_H
+
+#include "blip_buffer/blip_synth.hpp"
+
+struct Sms_Osc
+{
+	BLIPBuffer* outputs [4]; // NULL, right, left, center
+	BLIPBuffer* output;
+	int output_select;
+
+	int delay;
+	int last_amp;
+	int volume;
+
+	Sms_Osc();
+	void reset();
+};
+
+struct Sms_Square : Sms_Osc
+{
+	int period;
+	int phase;
+
+	typedef BLIPSynth<BLIPQuality::Good, 1> Synth;
+	const Synth* synth;
+
+	void reset();
+	void run( blip_time_t, blip_time_t );
+};
+
+struct Sms_Noise : Sms_Osc
+{
+	const int* period;
+	unsigned shifter;
+	unsigned feedback;
+
+	typedef BLIPSynth<BLIPQuality::Medium, 1> Synth;
+	Synth synth;
+
+	void reset();
+	void run( blip_time_t, blip_time_t );
+};
+
+#endif

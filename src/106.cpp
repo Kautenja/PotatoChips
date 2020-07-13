920917ed4e11a8f9b9ff6b80eca72d6b74a931e1
Kautenja <ckfour@icloud.com>
2020-07-13 18:30:11 -0500

    5 wavetables in module

--- src/106.cpp
+++ src/106.cpp
@@ -17,13 +17,13 @@
 
 #include "plugin.hpp"
 #include "components.hpp"
-#include "dsp/namco_106_apu.hpp"
 #include "widgets/wavetable_editor.hpp"
-#include <iostream>
+#include "dsp/namco_106_apu.hpp"
 
 /// Addresses to the registers for channel 1. To get channel \f$n\f$,
 /// multiply by \f$8n\f$.
 enum Namco106Registers {
+    REGS_PER_VOICE = 8,
     FREQ_LOW = 0x40,
     PHASE_LOW,
     FREQ_MEDIUM,
@@ -79,10 +79,40 @@ struct Chip106 : Module {
     static constexpr auto bit_depth = 15;
     /// the number of samples in the wave-table
     static constexpr auto num_samples = 32;
-    /// the samples in the wave-table
-    uint8_t values[num_samples] = {
+    /// the samples in the wave-table (1)
+    uint8_t values0[num_samples] = {
         0xA,0x8,0xD,0xC,0xE,0xE,0xF,0xF,0xF,0xF,0xE,0xF,0xD,0xE,0xA,0xC,
         0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
+    };
+    /// the samples in the wave-table (2)
+    uint8_t values1[num_samples] = {
+        0xA,0x8,0xD,0xC,0xE,0xE,0xF,0xF,0xF,0xF,0xE,0xF,0xD,0xE,0xA,0xC,
+        0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
+    };
+    /// the samples in the wave-table (3)
+    uint8_t values2[num_samples] = {
+        0xA,0x8,0xD,0xC,0xE,0xE,0xF,0xF,0xF,0xF,0xE,0xF,0xD,0xE,0xA,0xC,
+        0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
+    };
+    /// the samples in the wave-table (4)
+    uint8_t values3[num_samples] = {
+        0xA,0x8,0xD,0xC,0xE,0xE,0xF,0xF,0xF,0xF,0xE,0xF,0xD,0xE,0xA,0xC,
+        0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
+    };
+    /// the samples in the wave-table (5)
+    uint8_t values4[num_samples] = {
+        0xA,0x8,0xD,0xC,0xE,0xE,0xF,0xF,0xF,0xF,0xE,0xF,0xD,0xE,0xA,0xC,
+        0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
+    };
+    // the number of editors on the module
+    static constexpr int num_wavetables = 5;
+    /// the wave-tables to morph between
+    uint8_t* values[num_wavetables] = {
+        values0,
+        values1,
+        values2,
+        values3,
+        values4
     };
 
     /// Initialize a new 106 Chip module.
@@ -101,13 +131,13 @@ struct Chip106 : Module {
         apu.volume(3.f);
     }
 
-    /// Set the frequency for a given channel.
+    /// Return the frequency parameter for the given channel.
     ///
-    /// @param channel the channel to set the frequency for
-    /// @param num_channels the number of enabled channels in [1, 8]
-    /// @param wave_address the address of the waveform for the channel
+    /// @param channel the channel to get the frequency parameter for
+    /// @returns the frequency parameter for the given channel. This includes
+    /// the value of the knob and any CV modulation.
     ///
-    void setFrequency(int channel, uint8_t num_channels = 1, uint8_t wave_address = 0) {
+    inline uint32_t getFrequency(uint8_t channel) {
         // get the frequency of the oscillator from the parameter and CVs
         float pitch = params[PARAM_FREQ + channel].getValue() / 12.f;
         pitch += inputs[INPUT_VOCT + channel].getVoltage();
@@ -122,24 +152,21 @@ struct Chip106 : Module {
         freq *= (wave_length * 1 * 15.f * 65536.f) / CLOCK_RATE;
         // clamp within the legal bounds for the frequency value
         freq = rack::clamp(freq, 4.f, 262143.f);
-        // extract the low, medium, and high frequency register values
+        // convert the frequency to a 32-bit value
         auto freq18bit = static_cast<uint32_t>(freq);
-        // FREQUENCY LOW
-        uint8_t low = (freq18bit & 0b000000000011111111) >> 0;
-        apu.write_addr(FREQ_LOW + 8 * channel);
-        apu.write_data(0, low);
-        // FREQUENCY MEDIUM
-        uint8_t med = (freq18bit & 0b001111111100000000) >> 8;
-        apu.write_addr(FREQ_MEDIUM + 8 * channel);
-        apu.write_data(0, med);
-        // WAVEFORM LENGTH + FREQUENCY HIGH
-        uint8_t hig = (freq18bit & 0b110000000000000000) >> 16;
-        apu.write_addr(FREQ_HIGH + 8 * channel);
-        apu.write_data(0, (wave_length << 2) | hig);
-        // WAVE ADDRESS
-        apu.write_addr(WAVE_ADDRESS + 8 * channel);
-        apu.write_data(0, wave_address);
-        // VOLUME (and channel selection on channel 8, no effect elsewhere)
+        // OR the waveform length into the high 6 bits of "frequency Hi"
+        // register, which is the third bite, i.e. shift left 2 + 16
+        freq18bit |= wave_length << 18;
+        return freq18bit;
+    }
+
+    /// Return the volume parameter for the given channel.
+    ///
+    /// @param channel the channel to get the volume parameter for
+    /// @returns the volume parameter for the given channel. This includes
+    /// the value of the knob and any CV modulation.
+    ///
+    inline uint8_t getVolume(uint8_t channel) {
         // the minimal value for the volume width register
         static constexpr float VOLUME_MIN = 0;
         // the maximal value for the volume width register
@@ -147,19 +174,48 @@ struct Chip106 : Module {
         // get the volume from the parameter knob
         auto levelParam = params[PARAM_VOLUME + channel].getValue();
         // apply the control voltage to the volume
+        static constexpr float FM_SCALE = 0.5f;
         if (inputs[INPUT_VOLUME + channel].isConnected())
-            levelParam *= inputs[INPUT_VOLUME + channel].getVoltage() / 2.f;
+            levelParam *= FM_SCALE * inputs[INPUT_VOLUME + channel].getVoltage();
         // get the 8-bit volume clamped within legal limits
-        uint8_t volume = rack::clamp(levelParam, VOLUME_MIN, VOLUME_MAX);
-        apu.write_addr(VOLUME + 8 * channel);
-        apu.write_data(0, ((num_channels - 1) << 4) | volume);
+        return rack::clamp(levelParam, VOLUME_MIN, VOLUME_MAX);
+    }
+
+    /// Set the frequency for a given channel.
+    ///
+    /// @param channel the channel to set the frequency for
+    /// @param channels the number of enabled channels in [1, 8]
+    /// @param address the address of the waveform for the channel
+    ///
+    void setFrequency(uint8_t channel, uint8_t channels = 1, uint8_t address = 0) {
+        // extract the low, medium, and high frequency register values
+        auto freq = getFrequency(channel);
+        // FREQUENCY LOW
+        uint8_t low = (freq & 0b000000000000000011111111) >> 0;
+        apu.write_addr(FREQ_LOW + REGS_PER_VOICE * channel);
+        apu.write_data(0, low);
+        // FREQUENCY MEDIUM
+        uint8_t med = (freq & 0b000000001111111100000000) >> 8;
+        apu.write_addr(FREQ_MEDIUM + REGS_PER_VOICE * channel);
+        apu.write_data(0, med);
+        // WAVEFORM LENGTH + FREQUENCY HIGH
+        uint8_t hig = (freq & 0b111111110000000000000000) >> 16;
+        apu.write_addr(FREQ_HIGH + REGS_PER_VOICE * channel);
+        apu.write_data(0, hig);
+        // WAVE ADDRESS
+        apu.write_addr(WAVE_ADDRESS + REGS_PER_VOICE * channel);
+        apu.write_data(0, address);
+        // VOLUME (and channel selection on channel 8, this has no effect on
+        // other channels, so the check logic is skipped)
+        apu.write_addr(VOLUME + REGS_PER_VOICE * channel);
+        apu.write_data(0, ((channels - 1) << 4) | getVolume(channel));
     }
 
     /// Return a 10V signed sample from the chip.
     ///
     /// @param channel the channel to get the audio sample for
     ///
-    float getAudioOut(int channel) {
+    float getAudioOut(uint8_t channel) {
         // the peak to peak output of the voltage
         static constexpr float Vpp = 10.f;
         // the amount of voltage per increment of 16-bit fidelity volume
@@ -190,7 +246,7 @@ struct Chip106 : Module {
         // write the waveform data to the RAM
         for (int i = 0; i < num_samples / 2; i++) {
             apu.write_addr(i);
-            apu.write_data(0, (values[2 * i] << 4) | values[2 * i + 1]);
+            apu.write_data(0, (values[0][2 * i] << 4) | values[0][2 * i + 1]);
         }
         // get the number of active channels
         int num_channels = rack::math::clamp(
@@ -215,19 +271,26 @@ struct Chip106 : Module {
     /// Convert the module's state to a JSON object.
     json_t* dataToJson() override {
         json_t* rootJ = json_object();
-        json_t* array = json_array();
-        for (int i = 0; i < num_samples; i++)
-            json_array_append_new(array, json_integer(values[i]));
-        json_object_set_new(rootJ, "values", array);
+        for (int table = 0; table < num_wavetables; table++) {
+            json_t* array = json_array();
+            for (int sample = 0; sample < num_samples; sample++)
+                json_array_append_new(array, json_integer(values[table][sample]));
+            auto key = "values" + std::to_string(table);
+            json_object_set_new(rootJ, key.c_str(), array);
+        }
+
         return rootJ;
     }
 
     /// Load the module's state from a JSON object.
     void dataFromJson(json_t* rootJ) override {
-        json_t* data = json_object_get(rootJ, "values");
-        if (data) {
-            for (int i = 0; i < num_samples; i++)
-                values[i] = json_integer_value(json_array_get(data, i));
+        for (int table = 0; table < num_wavetables; table++) {
+            auto key = "values" + std::to_string(table);
+            json_t* data = json_object_get(rootJ, key.c_str());
+            if (data) {
+                for (int sample = 0; sample < num_samples; sample++)
+                    values[table][sample] = json_integer_value(json_array_get(data, sample));
+            }
         }
     }
 };
@@ -254,10 +317,8 @@ struct Chip106Widget : ModuleWidget {
             0x5,0x8,0x2,0x3,0x1,0x1,0x0,0x0,0x0,0x0,0x1,0x0,0x2,0x1,0x5,0x3
         };
         auto module_ = reinterpret_cast<Chip106*>(this->module);
-        // the number of editors on the module
-        static constexpr int num_editors = 5;
         // the fill colors for the wave-ttable editor lines
-        static constexpr NVGcolor colors[num_editors] = {
+        static constexpr NVGcolor colors[Chip106::num_wavetables] = {
             {{{1.f, 0.f, 0.f, 1.f}}},  // red
             {{{0.f, 1.f, 0.f, 1.f}}},  // green
             {{{0.f, 0.f, 1.f, 1.f}}},  // blue
@@ -265,9 +326,9 @@ struct Chip106Widget : ModuleWidget {
             {{{1.f, 1.f, 1.f, 1.f}}}   // white
         };
         // add wave-table editors
-        for (int i = 0; i < num_editors; i++) {
+        for (int i = 0; i < Chip106::num_wavetables; i++) {
             // get the wavetable buffer for this editor
-            uint8_t* wavetable = module ? &module_->values[0] : &default_values[0];
+            uint8_t* wavetable = module ? &module_->values[i][0] : &default_values[0];
             // setup a table editor for the buffer
             auto table_editor = new WaveTableEditor<uint8_t>(
                 wavetable,             // wave-table buffer

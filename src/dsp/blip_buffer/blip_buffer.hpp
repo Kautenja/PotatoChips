f0112b565a155ae48b7e58620bb40c6745776c87
Kautenja <ckfour@icloud.com>
2020-07-17 17:56:59 -0500

    inline blip_eq_t

--- src/dsp/blip_buffer/blip_buffer.hpp
+++ src/dsp/blip_buffer/blip_buffer.hpp
@@ -446,37 +446,32 @@ class blip_eq_t {
     /// increase treble.
     /// @param cutoff TODO:
     ///
-    static void gen_sinc(
+    static inline void gen_sinc(
         float* out,
         uint32_t count,
         double oversample,
         double treble,
         double cutoff
     ) {
-        if (cutoff >= 0.999)
-            cutoff = 0.999;
-        if (treble < -300.0)
-            treble = -300.0;
-        if (treble > 5.0)
-            treble = 5.0;
-
+        if (cutoff >= 0.999) cutoff = 0.999;
+        if (treble < -300.0) treble = -300.0;
+        if (treble > 5.0)    treble = 5.0;
         static constexpr double maxh = 4096.0;
-        double const rolloff = pow(10.0, 1.0 / (maxh * 20.0) * treble / (1.0 - cutoff));
-        double const pow_a_n = pow(rolloff, maxh - maxh * cutoff);
-        double const to_angle = pi / 2 / maxh / oversample;
+        const double rolloff = pow(10.0, 1.0 / (maxh * 20.0) * treble / (1.0 - cutoff));
+        const double pow_a_n = pow(rolloff, maxh - maxh * cutoff);
+        const double to_angle = pi / 2 / maxh / oversample;
         for (uint32_t i = 0; i < count; i++) {
             double angle = ((i - count) * 2 + 1) * to_angle;
             double c = rolloff * cos((maxh - 1.0) * angle) - cos(maxh * angle);
             double cos_nc_angle = cos(maxh * cutoff * angle);
             double cos_nc1_angle = cos((maxh * cutoff - 1.0) * angle);
             double cos_angle = cos(angle);
-
             c = c * pow_a_n - rolloff * cos_nc1_angle + cos_nc_angle;
             double d = 1.0 + rolloff * (rolloff - cos_angle - cos_angle);
             double b = 2.0 - cos_angle - cos_angle;
             double a = 1.0 - cos_angle - cos_nc_angle + cos_nc1_angle;
-
-            out[i] = (float) ((a * d + c * b) / (b * d)); // a / b + c / d
+            // a / b + c / d
+            out[i] = (float) ((a * d + c * b) / (b * d));
         }
     }
 
@@ -487,7 +482,7 @@ class blip_eq_t {
     /// @details
     /// for usage within instances of BLIPSynth_
     ///
-    void generate(float* out, uint32_t count) const {
+    inline void generate(float* out, uint32_t count) const {
         // lower cutoff freq for narrow kernels with their wider transition band
         // (8 points->1.49, 16 points->1.15)
         // double oversample = blip_res * 2.25 / count + 0.85;

a148e4c006a92dd94828340d6c990aebb3c18949
Kautenja <ckfour@icloud.com>
2020-07-16 21:52:09 -0500

    simplify blargg_common

--- src/dsp/blargg_common.h
+++ src/dsp/blargg_common.h
@@ -4,37 +4,21 @@
 #ifndef BLARGG_COMMON_H_
 #define BLARGG_COMMON_H_
 
-// Uncomment to use zlib for transparent decompression of gzipped files
-//#define HAVE_ZLIB_H
-// Uncomment to support only the listed game music types. See gme_type_list.cpp
-// for a list of all types.
-//#define GME_TYPE_LIST gme_nsf_type, gme_gbs_type
 // Uncomment to enable platform-specific optimizations
 //#define BLARGG_NONPORTABLE 1
 // Uncomment to use faster, lower quality sound synthesis
 //#define BLIP_BUFFER_FAST 1
 // Uncomment if automatic byte-order determination doesn't work
-//#define BLARGG_BIG_ENDIAN 1
-// Uncomment if you get errors in the bool section of blargg_common.h
-//#define BLARGG_COMPILER_HAS_BOOL 1
+// #define BLARGG_BIG_ENDIAN 1
 
 #include <cstddef>
 #include <cstdlib>
 #include <cassert>
 #include <climits>
-#include <climits>
 #include <cstdint>
 
-#if INT_MAX >= 0x7FFFFFFF
-	typedef int blargg_long;
-#else
-	typedef long blargg_long;
-#endif
+typedef int32_t blargg_long;
 
-#if UINT_MAX >= 0xFFFFFFFF
-	typedef unsigned blargg_ulong;
-#else
-	typedef unsigned long blargg_ulong;
-#endif
+typedef uint32_t blargg_ulong;
 
 #endif  // BLARGG_COMMON_H_

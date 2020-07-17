edd3202fecdceaa6dca4b7889712398892874d01
Kautenja <ckfour@icloud.com>
2020-07-17 17:49:07 -0500

    refactor scope of gen_sinc to class static of blip_eq_t move BLARGG_PI to class scope too as constexpr `pi`

--- src/dsp/blip_buffer/blip_buffer.hpp
+++ src/dsp/blip_buffer/blip_buffer.hpp
@@ -17,7 +17,6 @@
 #ifndef BLIP_BUFFER_HPP_
 #define BLIP_BUFFER_HPP_
 
-#include "sinc.hpp"
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
@@ -425,6 +424,8 @@ class blip_eq_t {
  private:
     /// BLIPSynth_ is a friend to access the private generate function
     friend class BLIPSynth_;
+    /// the constant value for Pi
+    static constexpr double pi = 3.1415926535897932384626433832795029;
     /// Logarithmic roll-off to treble dB at half sampling rate. Negative
     /// values reduce treble, small positive values (0 to 5.0) increase treble.
     double treble;
@@ -434,6 +435,50 @@ class blip_eq_t {
     uint32_t sample_rate;
     /// TODO:
     uint32_t cutoff_freq;
+
+    /// Generate a sinc.
+    ///
+    /// @param out TODO:
+    /// @param count TODO:
+    /// @param oversample TODO:
+    /// @param treble Logarithmic roll-off to treble dB at half sampling rate.
+    /// Negative values reduce treble, small positive values (0 to 5.0)
+    /// increase treble.
+    /// @param cutoff TODO:
+    ///
+    static void gen_sinc(
+        float* out,
+        uint32_t count,
+        double oversample,
+        double treble,
+        double cutoff
+    ) {
+        if (cutoff >= 0.999)
+            cutoff = 0.999;
+        if (treble < -300.0)
+            treble = -300.0;
+        if (treble > 5.0)
+            treble = 5.0;
+
+        static constexpr double maxh = 4096.0;
+        double const rolloff = pow(10.0, 1.0 / (maxh * 20.0) * treble / (1.0 - cutoff));
+        double const pow_a_n = pow(rolloff, maxh - maxh * cutoff);
+        double const to_angle = pi / 2 / maxh / oversample;
+        for (uint32_t i = 0; i < count; i++) {
+            double angle = ((i - count) * 2 + 1) * to_angle;
+            double c = rolloff * cos((maxh - 1.0) * angle) - cos(maxh * angle);
+            double cos_nc_angle = cos(maxh * cutoff * angle);
+            double cos_nc1_angle = cos((maxh * cutoff - 1.0) * angle);
+            double cos_angle = cos(angle);
+
+            c = c * pow_a_n - rolloff * cos_nc1_angle + cos_nc_angle;
+            double d = 1.0 + rolloff * (rolloff - cos_angle - cos_angle);
+            double b = 2.0 - cos_angle - cos_angle;
+            double a = 1.0 - cos_angle - cos_nc_angle + cos_nc1_angle;
+
+            out[i] = (float) ((a * d + c * b) / (b * d)); // a / b + c / d
+        }
+    }
 
     /// Generate sinc values into an output buffer with given quantity.
     ///
@@ -453,7 +498,7 @@ class blip_eq_t {
         // generate a sinc
         gen_sinc(out, count, blip_res * oversample, treble, cutoff);
         // apply (half of) hamming window
-        double to_fraction = BLARGG_PI / (count - 1);
+        double to_fraction = pi / (count - 1);
         for (uint32_t i = count; i--;)
             out[i] *= 0.54f - 0.46f * static_cast<float>(cos(i * to_fraction));
     }

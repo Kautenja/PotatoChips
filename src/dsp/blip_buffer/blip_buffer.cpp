d3ec19198b2ecedd3a262ba260fd869dc37f2905
Kautenja <ckfour@icloud.com>
2020-07-17 00:39:39 -0500

    cleanup style

--- src/dsp/blip_buffer/blip_buffer.cpp
+++ src/dsp/blip_buffer/blip_buffer.cpp
@@ -160,7 +160,7 @@ static void gen_sinc(float* out, int count, double oversample, double treble, do
         double b = 2.0 - cos_angle - cos_angle;
         double a = 1.0 - cos_angle - cos_nc_angle + cos_nc1_angle;
 
-        out [i] = (float) ((a * d + c * b) / (b * d)); // a / b + c / d
+        out[i] = (float) ((a * d + c * b) / (b * d)); // a / b + c / d
     }
 }
 
@@ -178,7 +178,7 @@ void blip_eq_t::generate(float* out, int count) const {
     // apply (half of) hamming window
     double to_fraction = PI / (count - 1);
     for (int i = count; i--;)
-        out [i] *= 0.54f - 0.46f * (float) cos(i * to_fraction);
+        out[i] *= 0.54f - 0.46f * (float) cos(i * to_fraction);
 }
 
 void BLIPSynth_::adjust_impulse() {
@@ -188,35 +188,35 @@ void BLIPSynth_::adjust_impulse() {
         int p2 = blip_res - 2 - p;
         long error = kernel_unit;
         for (int i = 1; i < size; i += blip_res) {
-            error -= impulses [i + p ];
-            error -= impulses [i + p2];
+            error -= impulses[i + p ];
+            error -= impulses[i + p2];
         }
         if (p == p2)
             error /= 2; // phase = 0.5 impulse uses same half for both sides
-        impulses [size - blip_res + p] += (blip_sample_t) error;
+        impulses[size - blip_res + p] += (blip_sample_t) error;
     }
 }
 
 void BLIPSynth_::treble_eq(blip_eq_t const& eq) {
-    float fimpulse [blip_res / 2 * (blip_widest_impulse_ - 1) + blip_res * 2];
+    float fimpulse[blip_res / 2 * (blip_widest_impulse_ - 1) + blip_res * 2];
 
     int const half_size = blip_res / 2 * (width - 1);
-    eq.generate(&fimpulse [blip_res], half_size);
+    eq.generate(&fimpulse[blip_res], half_size);
 
     int i;
 
     // need mirror slightly past center for calculation
     for (i = blip_res; i--;)
-        fimpulse [blip_res + half_size + i] = fimpulse [blip_res + half_size - 1 - i];
+        fimpulse[blip_res + half_size + i] = fimpulse[blip_res + half_size - 1 - i];
 
     // starts at 0
     for (i = 0; i < blip_res; i++)
-        fimpulse [i] = 0.0f;
+        fimpulse[i] = 0.0f;
 
     // find rescale factor
     double total = 0.0;
     for (i = 0; i < half_size; i++)
-        total += fimpulse [blip_res + i];
+        total += fimpulse[blip_res + i];
 
     // double const base_unit = 44800.0 - 128 * 18; // allows treble up to +0 dB
     // double const base_unit = 37888.0; // allows treble to +5 dB
@@ -229,9 +229,9 @@ void BLIPSynth_::treble_eq(blip_eq_t const& eq) {
     double next = 0.0;
     int const impulses_size = this->impulses_size();
     for (i = 0; i < impulses_size; i++) {
-        impulses [i] = (blip_sample_t) floor((next - sum) * rescale + 0.5);
-        sum += fimpulse [i];
-        next += fimpulse [i + blip_res];
+        impulses[i] = (blip_sample_t) floor((next - sum) * rescale + 0.5);
+        sum += fimpulse[i];
+        next += fimpulse[i + blip_res];
     }
     adjust_impulse();
 
@@ -269,7 +269,7 @@ void BLIPSynth_::volume_unit(double new_unit) {
                 long offset = 0x8000 + (1 << (shift - 1));
                 long offset2 = 0x8000 >> shift;
                 for (int i = impulses_size(); i--;)
-                    impulses [i] = (blip_sample_t) (((impulses [i] + offset) >> shift) - offset2);
+                    impulses[i] = (blip_sample_t) (((impulses[i] + offset) >> shift) - offset2);
                 adjust_impulse();
             }
         }

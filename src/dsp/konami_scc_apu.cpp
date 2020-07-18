b69a6bcc33ad66a7798d51f26484281b2adee462
Kautenja <ckfour@icloud.com>
2020-07-18 01:11:21 -0500

    convert tabs to spaces

--- src/dsp/konami_scc_apu.cpp
+++ src/dsp/konami_scc_apu.cpp
@@ -26,74 +26,74 @@ int const wave_size = 0x20;
 
 void Scc_Apu::run_until( blip_time_t end_time )
 {
-	for ( int index = 0; index < osc_count; index++ )
-	{
-		osc_t& osc = oscs [index];
+    for ( int index = 0; index < osc_count; index++ )
+    {
+        osc_t& osc = oscs [index];
 
-		BLIPBuffer* const output = osc.output;
-		if ( !output )
-			continue;
+        BLIPBuffer* const output = osc.output;
+        if ( !output )
+            continue;
 
-		blip_time_t period = (regs [0x80 + index * 2 + 1] & 0x0F) * 0x100 +
-				regs [0x80 + index * 2] + 1;
-		int volume = 0;
-		if ( regs [0x8F] & (1 << index) )
-		{
-			blip_time_t inaudible_period = (blargg_ulong) (output->get_clock_rate() +
-					inaudible_freq * 32) / (inaudible_freq * 16);
-			if ( period > inaudible_period )
-				volume = (regs [0x8A + index] & 0x0F) * (amp_range / 256 / 15);
-		}
+        blip_time_t period = (regs [0x80 + index * 2 + 1] & 0x0F) * 0x100 +
+                regs [0x80 + index * 2] + 1;
+        int volume = 0;
+        if ( regs [0x8F] & (1 << index) )
+        {
+            blip_time_t inaudible_period = (blargg_ulong) (output->get_clock_rate() +
+                    inaudible_freq * 32) / (inaudible_freq * 16);
+            if ( period > inaudible_period )
+                volume = (regs [0x8A + index] & 0x0F) * (amp_range / 256 / 15);
+        }
 
-		int8_t const* wave = (int8_t*) regs + index * wave_size;
-		if ( index == osc_count - 1 )
-			wave -= wave_size; // last two oscs share wave
-		{
-			int amp = wave [osc.phase] * volume;
-			int delta = amp - osc.last_amp;
-			if ( delta )
-			{
-				osc.last_amp = amp;
-				synth.offset( last_time, delta, output );
-			}
-		}
+        int8_t const* wave = (int8_t*) regs + index * wave_size;
+        if ( index == osc_count - 1 )
+            wave -= wave_size; // last two oscs share wave
+        {
+            int amp = wave [osc.phase] * volume;
+            int delta = amp - osc.last_amp;
+            if ( delta )
+            {
+                osc.last_amp = amp;
+                synth.offset( last_time, delta, output );
+            }
+        }
 
-		blip_time_t time = last_time + osc.delay;
-		if ( time < end_time )
-		{
-			if ( !volume )
-			{
-				// maintain phase
-				blargg_long count = (end_time - time + period - 1) / period;
-				osc.phase = (osc.phase + count) & (wave_size - 1);
-				time += count * period;
-			}
-			else
-			{
+        blip_time_t time = last_time + osc.delay;
+        if ( time < end_time )
+        {
+            if ( !volume )
+            {
+                // maintain phase
+                blargg_long count = (end_time - time + period - 1) / period;
+                osc.phase = (osc.phase + count) & (wave_size - 1);
+                time += count * period;
+            }
+            else
+            {
 
-				int phase = osc.phase;
-				int last_wave = wave [phase];
-				phase = (phase + 1) & (wave_size - 1); // pre-advance for optimal inner loop
+                int phase = osc.phase;
+                int last_wave = wave [phase];
+                phase = (phase + 1) & (wave_size - 1); // pre-advance for optimal inner loop
 
-				do
-				{
-					int amp = wave [phase];
-					phase = (phase + 1) & (wave_size - 1);
-					int delta = amp - last_wave;
-					if ( delta )
-					{
-						last_wave = amp;
-						synth.offset( time, delta * volume, output );
-					}
-					time += period;
-				}
-				while ( time < end_time );
+                do
+                {
+                    int amp = wave [phase];
+                    phase = (phase + 1) & (wave_size - 1);
+                    int delta = amp - last_wave;
+                    if ( delta )
+                    {
+                        last_wave = amp;
+                        synth.offset( time, delta * volume, output );
+                    }
+                    time += period;
+                }
+                while ( time < end_time );
 
-				osc.phase = phase = (phase - 1) & (wave_size - 1); // undo pre-advance
-				osc.last_amp = wave [phase] * volume;
-			}
-		}
-		osc.delay = time - end_time;
-	}
-	last_time = end_time;
+                osc.phase = phase = (phase - 1) & (wave_size - 1); // undo pre-advance
+                osc.last_amp = wave [phase] * volume;
+            }
+        }
+        osc.delay = time - end_time;
+    }
+    last_time = end_time;
 }

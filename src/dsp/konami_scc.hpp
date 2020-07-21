378f9fcfbe42d6e1a2b739ea087d71b9f660f866
Kautenja <ckfour@icloud.com>
2020-07-20 23:42:56 -0500

    stub SCC

--- src/dsp/konami_scc.hpp
+++ src/dsp/konami_scc.hpp
@@ -1,4 +1,4 @@
-// Konami SCC sound chip emulator
+// Konami SCC sound chip emulator.
 // Copyright 2020 Christian Kauten
 // Copyright 2006 Shay Green
 //
@@ -23,99 +23,175 @@
 #include "blip_buffer.hpp"
 #include <cstring>
 
-class Scc_Apu {
-public:
-    // Set buffer to generate all sound into, or disable sound if NULL
-    void output(BLIPBuffer* );
+// TODO: remove blargg_ulong
+// TODO: c-cast to static_cast
 
-    // Reset sound chip
-    void reset();
+/// Konami SCC sound chip emulator.
+class KonamiSCC {
+ public:
+    /// TODO:
+    enum { OSC_COUNT = 5 };
+    /// TODO:
+    enum { REGISTER_COUNT = 0x90 };
 
-    // Write to register at specified time
-    enum { reg_count = 0x90 };
-    void write(blip_time_t time, int reg, int data );
+ private:
+    /// the range of the amplifier on the chip
+    enum { AMP_RANGE = 0x8000 };
+    /// Tones above this frequency are treated as disabled tone at half volume.
+    /// Power of two is more efficient (avoids division).
+    unsigned static constexpr INAUDIBLE_FREQ = AMP_RANGE / 2;
+    /// TODO:
+    int static constexpr WAVE_SIZE = 0x20;
 
-    // Run sound to specified time, end current time frame, then start a new
-    // time frame at time 0. Time frames have no effect on emulation and each
-    // can be whatever length is convenient.
-    void end_frame(blip_time_t length );
-
-// Additional features
-
-    // Set sound output of specific oscillator to buffer, where index is
-    // 0 to 4. If buffer is NULL, the specified oscillator is muted.
-    enum { osc_count = 5 };
-    void osc_output(int index, BLIPBuffer* );
-
-    // Set overall volume (default is 1.0)
-    void volume(double );
-
-    // Set treble equalization (see documentation)
-    void treble_eq(blip_eq_t const& );
-
-public:
-    Scc_Apu();
-private:
-    enum { amp_range = 0x8000 };
-    struct osc_t
-    {
-        int delay;
-        int phase;
-        int last_amp;
-        BLIPBuffer* output;
+    /// An oscillators on the chip.
+    struct osc_t {
+        /// TODO:
+        int delay = 0;
+        /// TODO:
+        int phase = 0;
+        /// TODO:
+        int last_amp = 0;
+        /// TODO:
+        BLIPBuffer* output = NULL;
     };
-    osc_t oscs [osc_count];
-    blip_time_t last_time;
-    unsigned char regs [reg_count];
+
+    /// the oscillators on the chip
+    osc_t oscs[OSC_COUNT];
+    /// the last time the oscillators were updated
+    blip_time_t last_time = 0;
+    /// the registers on the chip
+    unsigned char regs[REGISTER_COUNT];
+    /// the synthesizer for the oscillators on the chip
     BLIPSynth<blip_med_quality, 1> synth;
 
-    void run_until(blip_time_t );
+    /// Run the oscillators until the given end time.
+    ///
+    /// @param end_time the time to run the oscillators until
+    ///
+    void run_until(blip_time_t end_time) {
+        for (int index = 0; index < OSC_COUNT; index++) {
+            osc_t& osc = oscs[index];
+            // get the output buffer for this oscillator, continue if it's null
+            BLIPBuffer* const output = osc.output;
+            if (!output) continue;
+            // get the period of the oscillator
+            blip_time_t period = (regs[0x80 + index * 2 + 1] & 0x0F) * 0x100 + regs[0x80 + index * 2] + 1;
+            int volume = 0;
+            if (regs[0x8F] & (1 << index)) {
+                blip_time_t inaudible_period = (blargg_ulong) (output->get_clock_rate() + INAUDIBLE_FREQ * 32) / (INAUDIBLE_FREQ * 16);
+                if (period > inaudible_period)
+                    volume = (regs[0x8A + index] & 0x0F) * (AMP_RANGE / 256 / 15);
+            }
+            // get the wave for the oscillator
+            int8_t const* wave = (int8_t*) regs + index * WAVE_SIZE;
+            // the last two oscillators share a wave
+            if (index == OSC_COUNT - 1) wave -= WAVE_SIZE;
+            {  // confine scope of `amp` and `delta`
+                int amp = wave[osc.phase] * volume;
+                int delta = amp - osc.last_amp;
+                if (delta) {
+                    osc.last_amp = amp;
+                    synth.offset(last_time, delta, output);
+                }
+            }
+            // get the time to advance to
+            blip_time_t time = last_time + osc.delay;
+            if (time < end_time) {
+                if (!volume) {  // maintain phase
+                    blargg_long count = (end_time - time + period - 1) / period;
+                    osc.phase = (osc.phase + count) & (WAVE_SIZE - 1);
+                    time += count * period;
+                } else {
+                    int phase = osc.phase;
+                    int last_wave = wave[phase];
+                    // pre-advance for optimal inner loop
+                    phase = (phase + 1) & (WAVE_SIZE - 1);
+                    do {
+                        int amp = wave[phase];
+                        phase = (phase + 1) & (WAVE_SIZE - 1);
+                        int delta = amp - last_wave;
+                        if (delta) {
+                            last_wave = amp;
+                            synth.offset(time, delta * volume, output);
+                        }
+                        time += period;
+                    } while (time < end_time);
+                    // undo pre-advance
+                    osc.phase = phase = (phase - 1) & (WAVE_SIZE - 1);
+                    osc.last_amp = wave[phase] * volume;
+                }
+            }
+            osc.delay = time - end_time;
+        }
+        last_time = end_time;
+    }
+
+ public:
+    /// Initialize a new Konami SCC.
+    KonamiSCC() {
+        memset(regs, 0, sizeof regs);
+    }
+
+    /// Set overall volume of all oscillators, where 1.0 is full volume
+    ///
+    /// @param level the value to set the volume to
+    ///
+    inline void set_volume(double v) {
+        synth.volume(0.43 / OSC_COUNT / AMP_RANGE * v);
+    }
+
+    /// Set treble equalization for the synthesizers.
+    ///
+    /// @param equalizer the equalization parameter for the synthesizers
+    ///
+    inline void treble_eq(blip_eq_t const& equalizer) {
+        synth.treble_eq(equalizer);
+    }
+
+    /// Set sound output of specific oscillator to buffer, where index is
+    /// 0 to 4. If buffer is NULL, the specified oscillator is muted.
+    inline void set_output(int index, BLIPBuffer* buffer) {
+        assert((unsigned) index < OSC_COUNT);
+        oscs[index].output = buffer;
+    }
+
+    /// Set buffer to generate all sound into, or disable sound if NULL
+    inline void set_output(BLIPBuffer* buffer) {
+        for (int i = 0; i < OSC_COUNT; i++)
+            set_output(i, buffer);
+    }
+
+    /// Reset oscillators and internal state.
+    inline void reset() {
+        last_time = 0;
+        for (int i = 0; i < OSC_COUNT; i++)
+            memset(&oscs[i], 0, offsetof(osc_t, output));
+        memset(regs, 0, sizeof regs);
+    }
+
+    /// Write to the data port.
+    ///
+    /// @param addr the register to write the data to
+    /// @param data the byte to write to the register at given address
+    ///
+    inline void write(int addr, int data) {
+        assert((unsigned) addr < REGISTER_COUNT);
+        // TODO: remove this?
+        run_until(0);
+        regs[addr] = data;
+    }
+
+    /// Run all oscillators up to specified time, end current frame, then
+    /// start a new frame at time 0.
+    ///
+    /// @param end_time the time to run the oscillators until
+    ///
+    inline void end_frame(blip_time_t end_time) {
+        if (end_time > last_time)
+            run_until(end_time);
+        last_time -= end_time;
+        assert(last_time >= 0);
+    }
 };
-
-inline void Scc_Apu::volume(double v ) { synth.volume(0.43 / osc_count / amp_range * v ); }
-
-inline void Scc_Apu::treble_eq(blip_eq_t const& eq ) { synth.treble_eq(eq ); }
-
-inline void Scc_Apu::osc_output(int index, BLIPBuffer* b )
-{
-    assert((unsigned) index < osc_count );
-    oscs [index].output = b;
-}
-
-inline void Scc_Apu::write(blip_time_t time, int addr, int data )
-{
-    assert((unsigned) addr < reg_count );
-    run_until(time );
-    regs [addr] = data;
-}
-
-inline void Scc_Apu::end_frame(blip_time_t end_time )
-{
-    if (end_time > last_time )
-        run_until(end_time );
-    last_time -= end_time;
-    assert(last_time >= 0 );
-}
-
-inline void Scc_Apu::output(BLIPBuffer* buf )
-{
-    for (int i = 0; i < osc_count; i++ )
-        oscs [i].output = buf;
-}
-
-inline Scc_Apu::Scc_Apu()
-{
-    output(0 );
-}
-
-inline void Scc_Apu::reset()
-{
-    last_time = 0;
-
-    for (int i = 0; i < osc_count; i++ )
-        memset(&oscs [i], 0, offsetof (osc_t,output) );
-
-    memset(regs, 0, sizeof regs );
-}
 
 #endif  // DSP_KONAMI_SCC_HPP_

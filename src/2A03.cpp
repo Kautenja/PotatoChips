e01941253439b4fd790134cbb16d2e6d35696b7a
Kautenja <ckfour@icloud.com>
2020-07-22 13:16:32 -0500

    fix sample rate change response logic

--- src/2A03.cpp
+++ src/2A03.cpp
@@ -47,9 +47,6 @@ struct Chip2A03 : Module {
     /// The 2A03 instance to synthesize sound with
     Ricoh2A03 apu;
 
-    /// a signal flag for detecting sample rate changes
-    bool new_sample_rate = true;
-
     // a clock divider for running CV acquisition slower than audio rate
     dsp::ClockDivider cvDivider;
 
@@ -70,6 +67,7 @@ struct Chip2A03 : Module {
         for (int i = 0; i < Ricoh2A03::OSC_COUNT; i++) apu.osc_output(i, &buf[i]);
         // volume of 3 produces a roughly 5Vpp signal from all voices
         apu.volume(3.f);
+        onSampleRateChange();
     }
 
     /// Process pulse wave for given channel.
@@ -169,14 +167,6 @@ struct Chip2A03 : Module {
     void process(const ProcessArgs &args) override {
         // calculate the number of clock cycles on the chip per audio sample
         uint32_t cycles_per_sample = CLOCK_RATE / args.sampleRate;
-        // check for sample rate changes from the engine to send to the chip
-        if (new_sample_rate) {
-            // update the buffer for each channel
-            for (int i = 0; i < Ricoh2A03::OSC_COUNT; i++)
-                buf[i].set_sample_rate(args.sampleRate, CLOCK_RATE);
-            // clear the new sample rate flag
-            new_sample_rate = false;
-        }
         if (cvDivider.process()) {  // process the CV inputs to the chip
             lfsr.process(rescale(inputs[INPUT_LFSR].getVoltage(), 0.f, 2.f, 0.f, 1.f));
             // process the data on the chip
@@ -194,7 +184,11 @@ struct Chip2A03 : Module {
     }
 
     /// Respond to the change of sample rate in the engine.
-    inline void onSampleRateChange() override { new_sample_rate = true; }
+    inline void onSampleRateChange() override {
+        // update the buffer for each channel
+        for (int i = 0; i < Ricoh2A03::OSC_COUNT; i++)
+            buf[i].set_sample_rate(APP->engine->getSampleRate(), CLOCK_RATE);
+    }
 };
 
 // ---------------------------------------------------------------------------

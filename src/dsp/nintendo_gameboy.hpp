711752f118f09e205635309028c1d874ffa71932
Kautenja <ckfour@icloud.com>
2020-07-23 02:37:25 -0500

    cleanup GBS oscillator code

--- src/dsp/nintendo_gameboy.hpp
+++ src/dsp/nintendo_gameboy.hpp
@@ -34,6 +34,15 @@ const uint8_t sine_wave[32] = {
 /// The Nintendo GameBoy Sound System (GBS) Audio Processing Unit (APU).
 class NintendoGBS {
  public:
+    /// the number of oscillators on the chip
+    enum { OSC_COUNT = 4 };
+    /// the first address of the APU in memory space
+    enum { ADDR_START = 0xFF10 };
+    /// the last address of the APU in memory space
+    enum { ADDR_END   = 0xFF3F };
+    /// the total number of registers available on the chip
+    enum { REGISTER_COUNT = ADDR_END - ADDR_START + 1 };
+
     /// Registers for the Nintendo GameBoy Sound System (GBS) APU.
     enum Registers {
         // Pulse 0
@@ -68,16 +77,139 @@ class NintendoGBS {
         WAVE_TABLE_VALUES                 = 0xFF30
     };
 
+ private:
+    /// TODO:
+    blip_time_t next_frame_time;
+    /// TODO:
+    blip_time_t last_time;
+    /// TODO:
+    blip_time_t frame_period;
+    /// TODO:
+    double volume_unit;
+    /// TODO:
+    int frame_count;
+
+    /// the pulse waveform generator for channel 1
+    NintendoGBS_Pulse pulse1;
+    /// the pulse waveform generator for channel 2
+    NintendoGBS_Pulse pulse2;
+    /// the DPCM waveform generator for channel 3
+    NintendoGBS_Wave wave;
+    /// the noise generator for channel 4
+    NintendoGBS_Noise noise;
+    /// the general set of oscillators on the chip
+    NintendoGBS_Oscillator* const oscs[OSC_COUNT] = {
+        &pulse1,
+        &pulse2,
+        &wave,
+        &noise
+    };
+    /// the registers on the chip
+    uint8_t regs[REGISTER_COUNT];
+
+    /// the synthesizer used by pulse waves
+    NintendoGBS_Pulse::Synth pulse_synth;
+    /// the synthesizer used by DPCM wave and noise
+    NintendoGBS_Wave::Synth other_synth;
+
+    void update_volume() {
+        // TODO: doesn't handle differing left/right global volume (support would
+        // require modification to all oscillator code)
+        int data = regs[STEREO_VOLUME - ADDR_START];
+        double vol = (std::max(data & 7, data >> 4 & 7) + 1) * volume_unit;
+        pulse_synth.volume(vol);
+        other_synth.volume(vol);
+    }
+
+    void run_until(blip_time_t end_time) {
+        assert(end_time >= last_time); // end_time must not be before previous time
+        if (end_time == last_time)
+            return;
+
+        while (true) {
+            blip_time_t time = next_frame_time;
+            if (time > end_time)
+                time = end_time;
+
+            // run oscillators
+            for (int i = 0; i < OSC_COUNT; ++i) {
+                NintendoGBS_Oscillator& osc = *oscs[i];
+                if (osc.output) {
+                    int playing = false;
+                    if (osc.enabled && osc.volume &&
+                            (!(osc.regs[4] & osc.len_enabled_mask) || osc.length))
+                        playing = -1;
+                    switch (i) {
+                    case 0: pulse1.run(last_time, time, playing); break;
+                    case 1: pulse2.run(last_time, time, playing); break;
+                    case 2: wave.run(  last_time, time, playing); break;
+                    case 3: noise.run( last_time, time, playing); break;
+                    }
+                }
+            }
+            last_time = time;
+
+            if (time == end_time)
+                break;
+
+            next_frame_time += frame_period;
+
+            // 256 Hz actions
+            pulse1.clock_length();
+            pulse2.clock_length();
+            wave.clock_length();
+            noise.clock_length();
+
+            frame_count = (frame_count + 1) & 3;
+            if (frame_count == 0) {
+                // 64 Hz actions
+                pulse1.clock_envelope();
+                pulse2.clock_envelope();
+                noise.clock_envelope();
+            }
+
+            if (frame_count & 1)
+                pulse1.clock_sweep(); // 128 Hz action
+        }
+    }
+
+    void write_osc(int index, int reg, int data) {
+        reg -= index * 5;
+        NintendoGBS_Pulse* sq = &pulse2;
+        switch (index) {
+        case 0:
+            sq = &pulse1;
+        case 1:
+            if (sq->write_register(reg, data) && index == 0) {
+                pulse1.sweep_freq = pulse1.frequency();
+                if ((regs[0] & sq->period_mask) && (regs[0] & sq->shift_mask)) {
+                    pulse1.sweep_delay = 1;  // cause sweep to recalculate now
+                    pulse1.clock_sweep();
+                }
+            }
+            break;
+        case 2:
+            wave.write_register(reg, data);
+            break;
+        case 3:
+            if (noise.write_register(reg, data))
+                noise.bits = 0x7FFF;
+        }
+    }
+
+    /// Disable the copy constructor
+    NintendoGBS(const NintendoGBS&);
+
+    /// Disable the assignment operator
+    NintendoGBS& operator=(const NintendoGBS&);
+
+ public:
+    /// Initialize a new Nintendo GBS chip emulator.
     NintendoGBS() {
-        square1.synth = &square_synth;
-        square2.synth = &square_synth;
+        pulse1.synth = &pulse_synth;
+        pulse2.synth = &pulse_synth;
         wave.synth  = &other_synth;
         noise.synth = &other_synth;
-
-        oscs[0] = &square1;
-        oscs[1] = &square2;
-        oscs[2] = &wave;
-        oscs[3] = &noise;
 
         for (int i = 0; i < OSC_COUNT; i++) {
             NintendoGBS_Oscillator& osc = *oscs[i];
@@ -105,7 +237,7 @@ class NintendoGBS {
 
     // Set treble equalization
     void treble_eq(const blip_eq_t& eq) {
-        square_synth.treble_eq(eq);
+        pulse_synth.treble_eq(eq);
         other_synth.treble_eq(eq);
     }
 
@@ -125,7 +257,6 @@ class NintendoGBS {
     // Assign single oscillator output to buffer(s). Valid indicies are 0 to 3,
     // which refer to Square 1, Square 2, Wave, and Noise. If buffer is NULL,
     // silences oscillator.
-    enum { OSC_COUNT = 4 };
     inline void osc_output(int index, BLIPBuffer* center, BLIPBuffer* left, BLIPBuffer* right) {
         assert((unsigned) index < OSC_COUNT);
         assert((center && left && right) || (!center && !left && !right));
@@ -146,8 +277,8 @@ class NintendoGBS {
         last_time       = 0;
         frame_count     = 0;
 
-        square1.reset();
-        square2.reset();
+        pulse1.reset();
+        pulse2.reset();
         wave.reset();
         noise.reset();
         noise.bits = 1;
@@ -166,11 +297,6 @@ class NintendoGBS {
         };
         memcpy(wave.wave, initial_wave, sizeof wave.wave);
     }
-
-    // Reads and writes at addr must satisfy ADDR_START <= addr <= ADDR_END
-    enum { ADDR_START = 0xFF10 };
-    enum { ADDR_END   = 0xFF3F };
-    enum { REGISTER_COUNT = ADDR_END - ADDR_START + 1 };
 
     // Write 'data' to address at specified time
     void write(unsigned addr, int data) {
@@ -294,119 +420,6 @@ class NintendoGBS {
         if (tempo_division != 1.0)
             frame_period = blip_time_t (frame_period / tempo_division);
     }
-
- private:
-    // noncopyable
-    NintendoGBS(const NintendoGBS&);
-    NintendoGBS& operator = (const NintendoGBS&);
-
-    NintendoGBS_Oscillator* oscs[OSC_COUNT];
-    blip_time_t next_frame_time;
-    blip_time_t last_time;
-    blip_time_t frame_period;
-    double volume_unit;
-    int frame_count;
-
-    NintendoGBS_Pulse square1;
-    NintendoGBS_Pulse square2;
-    NintendoGBS_Wave wave;
-    NintendoGBS_Noise noise;
-    uint8_t regs[REGISTER_COUNT];
-    // used by squares
-    NintendoGBS_Pulse::Synth square_synth;
-    // used by wave and noise
-    NintendoGBS_Wave::Synth other_synth;
-
-    void update_volume() {
-        // TODO: doesn't handle differing left/right global volume (support would
-        // require modification to all oscillator code)
-        int data = regs[STEREO_VOLUME - ADDR_START];
-        double vol = (std::max(data & 7, data >> 4 & 7) + 1) * volume_unit;
-        square_synth.volume(vol);
-        other_synth.volume(vol);
-    }
-
-    void run_until(blip_time_t end_time) {
-        assert(end_time >= last_time); // end_time must not be before previous time
-        if (end_time == last_time)
-            return;
-
-        while (true) {
-            blip_time_t time = next_frame_time;
-            if (time > end_time)
-                time = end_time;
-
-            // run oscillators
-            for (int i = 0; i < OSC_COUNT; ++i) {
-                NintendoGBS_Oscillator& osc = *oscs[i];
-                if (osc.output) {
-                    int playing = false;
-                    if (osc.enabled && osc.volume &&
-                            (!(osc.regs[4] & osc.len_enabled_mask) || osc.length))
-                        playing = -1;
-                    switch (i) {
-                    case 0: square1.run(last_time, time, playing); break;
-                    case 1: square2.run(last_time, time, playing); break;
-                    case 2: wave   .run(last_time, time, playing); break;
-                    case 3: noise  .run(last_time, time, playing); break;
-                    }
-                }
-            }
-            last_time = time;
-
-            if (time == end_time)
-                break;
-
-            next_frame_time += frame_period;
-
-            // 256 Hz actions
-            square1.clock_length();
-            square2.clock_length();
-            wave.clock_length();
-            noise.clock_length();
-
-            frame_count = (frame_count + 1) & 3;
-            if (frame_count == 0) {
-                // 64 Hz actions
-                square1.clock_envelope();
-                square2.clock_envelope();
-                noise.clock_envelope();
-            }
-
-            if (frame_count & 1)
-                square1.clock_sweep(); // 128 Hz action
-        }
-    }
-
-    void write_osc(int index, int reg, int data);
 };
-
-#include "nintendo_gameboy_oscillators.hpp"
-
-void NintendoGBS::write_osc(int index, int reg, int data) {
-    reg -= index * 5;
-    NintendoGBS_Pulse* sq = &square2;
-    switch (index) {
-    case 0:
-        sq = &square1;
-    case 1:
-        if (sq->write_register(reg, data) && index == 0) {
-            square1.sweep_freq = square1.frequency();
-            if ((regs[0] & sq->period_mask) && (regs[0] & sq->shift_mask)) {
-                square1.sweep_delay = 1;  // cause sweep to recalculate now
-                square1.clock_sweep();
-            }
-        }
-        break;
-
-    case 2:
-        wave.write_register(reg, data);
-        break;
-
-    case 3:
-        if (noise.write_register(reg, data))
-            noise.bits = 0x7FFF;
-    }
-}
 
 #endif  // DSP_NINTENDO_GAMEBOY_HPP_

9b79bfa33bee448a04ad8365fdebc8c8f7615eeb
Kautenja <ckfour@icloud.com>
2020-07-23 02:16:23 -0500

    resolve linter warnings

--- src/dsp/nintendo_gameboy_oscillators.cpp
+++ src/dsp/nintendo_gameboy_oscillators.cpp
@@ -24,11 +24,11 @@ void NintendoGBS_Oscillator::reset() {
     last_amp = 0;
     length = 0;
     output_select = 3;
-    output = outputs [output_select];
+    output = outputs[output_select];
 }
 
 void NintendoGBS_Oscillator::clock_length() {
-    if ((regs [4] & len_enabled_mask) && length)
+    if ((regs[4] & len_enabled_mask) && length)
         length--;
 }
 
@@ -36,8 +36,8 @@ void NintendoGBS_Oscillator::clock_length() {
 
 void NintendoGBS_Envelope::clock_envelope() {
     if (env_delay && !--env_delay) {
-        env_delay = regs [2] & 7;
-        int v = volume - 1 + (regs [2] >> 2 & 2);
+        env_delay = regs[2] & 7;
+        int v = volume - 1 + (regs[2] >> 2 & 2);
         if ((unsigned) v < 15)
             volume = v;
     }
@@ -46,7 +46,7 @@ void NintendoGBS_Envelope::clock_envelope() {
 bool NintendoGBS_Envelope::write_register(int reg, int data) {
     switch (reg) {
     case 1:
-        length = 64 - (regs [1] & 0x3F);
+        length = 64 - (regs[1] & 0x3F);
         break;
     case 2:
         if (!(data >> 4))
@@ -54,8 +54,8 @@ bool NintendoGBS_Envelope::write_register(int reg, int data) {
         break;
     case 4:
         if (data & trigger) {
-            env_delay = regs [2] & 7;
-            volume = regs [2] >> 4;
+            env_delay = regs[2] & 7;
+            volume = regs[2] >> 4;
             enabled = true;
             if (length == 0)
                 length = 64;
@@ -75,22 +75,22 @@ void NintendoGBS_Pulse::reset() {
 }
 
 void NintendoGBS_Pulse::clock_sweep() {
-    int sweep_period = (regs [0] & period_mask) >> 4;
+    int sweep_period = (regs[0] & period_mask) >> 4;
     if (sweep_period && sweep_delay && !--sweep_delay) {
         sweep_delay = sweep_period;
-        regs [3] = sweep_freq & 0xFF;
-        regs [4] = (regs [4] & ~0x07) | (sweep_freq >> 8 & 0x07);
+        regs[3] = sweep_freq & 0xFF;
+        regs[4] = (regs[4] & ~0x07) | (sweep_freq >> 8 & 0x07);
 
-        int offset = sweep_freq >> (regs [0] & shift_mask);
-        if (regs [0] & 0x08)
+        int offset = sweep_freq >> (regs[0] & shift_mask);
+        if (regs[0] & 0x08)
             offset = -offset;
         sweep_freq += offset;
 
         if (sweep_freq < 0) {
             sweep_freq = 0;
         } else if (sweep_freq >= 2048) {
-            sweep_delay = 0; // don't modify channel frequency any further
-            sweep_freq = 2048; // silence sound immediately
+            sweep_delay = 0;  // don't modify channel frequency any further
+            sweep_freq = 2048;  // silence sound immediately
         }
     }
 }
@@ -99,15 +99,14 @@ void NintendoGBS_Pulse::run(blip_time_t time, blip_time_t end_time, int playing)
     if (sweep_freq == 2048)
         playing = false;
 
-    static unsigned char const table [4] = { 1, 2, 4, 6 };
-    int const duty = table [regs [1] >> 6];
+    static unsigned char const table[4] = { 1, 2, 4, 6 };
+    int const duty = table[regs[1] >> 6];
     int amp = volume & playing;
     if (phase >= duty)
         amp = -amp;
 
     int frequency = this->frequency();
-    if (unsigned (frequency - 1) > 2040) // frequency < 1 || frequency > 2041
-    {
+    if (unsigned (frequency - 1) > 2040) {  // frequency < 1 || frequency > 2041
         // really high frequency results in DC at half volume
         amp = volume >> 1;
         playing = false;
@@ -130,18 +129,14 @@ void NintendoGBS_Pulse::run(blip_time_t time, blip_time_t end_time, int playing)
         BLIPBuffer* const output = this->output;
         int phase = this->phase;
         int delta = amp * 2;
-        do
-        {
+        do {
             phase = (phase + 1) & 7;
-            if (phase == 0 || phase == duty)
-            {
+            if (phase == 0 || phase == duty) {
                 delta = -delta;
                 synth->offset(time, delta, output);
             }
             time += period;
-        }
-        while (time < end_time);
-
+        } while (time < end_time);
         this->phase = phase;
         last_amp = delta >> 1;
     }
@@ -152,7 +147,7 @@ void NintendoGBS_Pulse::run(blip_time_t time, blip_time_t end_time, int playing)
 
 void NintendoGBS_Noise::run(blip_time_t time, blip_time_t end_time, int playing) {
     int amp = volume & playing;
-    int tap = 13 - (regs [3] & 8);
+    int tap = 13 - (regs[3] & 8);
     if (bits >> tap & 2)
         amp = -amp;
 
@@ -169,8 +164,8 @@ void NintendoGBS_Noise::run(blip_time_t time, blip_time_t end_time, int playing)
         time = end_time;
 
     if (time < end_time) {
-        static unsigned char const table [8] = { 8, 16, 32, 48, 64, 80, 96, 112 };
-        int period = table [regs [3] & 7] << (regs [3] >> 4);
+        static unsigned char const table[8] = { 8, 16, 32, 48, 64, 80, 96, 112 };
+        int period = table[regs[3] & 7] << (regs[3] >> 4);
 
         // keep parallel resampled time to eliminate time conversion in the loop
         BLIPBuffer* const output = this->output;
@@ -179,20 +174,17 @@ void NintendoGBS_Noise::run(blip_time_t time, blip_time_t end_time, int playing)
         unsigned bits = this->bits;
         int delta = amp * 2;
 
-        do
-        {
+        do {
             unsigned changed = (bits >> tap) + 1;
             time += period;
             bits <<= 1;
-            if (changed & 2)
-            {
+            if (changed & 2) {
                 delta = -delta;
                 bits |= 1;
                 synth->offset_resampled(resampled_time, delta, output);
             }
             resampled_time += resampled_period;
-        }
-        while (time < end_time);
+        } while (time < end_time);
 
         this->bits = bits;
         last_amp = delta >> 1;
@@ -208,17 +200,14 @@ inline void NintendoGBS_Wave::write_register(int reg, int data) {
         if (!(data & 0x80))
             enabled = false;
         break;
-
     case 1:
-        length = 256 - regs [1];
+        length = 256 - regs[1];
         break;
-
     case 2:
         volume = data >> 5 & 3;
         break;
-
     case 4:
-        if (data & trigger & regs [0]) {
+        if (data & trigger & regs[0]) {
             wave_pos = 0;
             enabled = true;
             if (length == 0)
@@ -228,14 +217,12 @@ inline void NintendoGBS_Wave::write_register(int reg, int data) {
 }
 
 void NintendoGBS_Wave::run(blip_time_t time, blip_time_t end_time, int playing) {
-    int volume_shift = (volume - 1) & 7; // volume = 0 causes shift = 7
+    int volume_shift = (volume - 1) & 7;  // volume = 0 causes shift = 7
     int frequency;
     {
-        int amp = (wave [wave_pos] >> volume_shift & playing) * 2;
-
+        int amp = (wave[wave_pos] >> volume_shift & playing) * 2;
         frequency = this->frequency();
-        if (unsigned (frequency - 1) > 2044) // frequency < 1 || frequency > 2045
-        {
+        if (unsigned (frequency - 1) > 2044) {  // frequency < 1 || frequency > 2045
             amp = 30 >> volume_shift & playing;
             playing = false;
         }
@@ -256,19 +243,16 @@ void NintendoGBS_Wave::run(blip_time_t time, blip_time_t end_time, int playing) 
         int const period = (2048 - frequency) * 2;
         int wave_pos = (this->wave_pos + 1) & (wave_size - 1);
 
-        do
-        {
-            int amp = (wave [wave_pos] >> volume_shift) * 2;
+        do {
+            int amp = (wave[wave_pos] >> volume_shift) * 2;
             wave_pos = (wave_pos + 1) & (wave_size - 1);
             int delta = amp - last_amp;
-            if (delta)
-            {
+            if (delta) {
                 last_amp = amp;
                 synth->offset(time, delta, output);
             }
             time += period;
-        }
-        while (time < end_time);
+        } while (time < end_time);
 
         this->wave_pos = (wave_pos - 1) & (wave_size - 1);
     }
@@ -286,9 +270,8 @@ void NintendoGBS::write_osc(int index, int reg, int data) {
     case 1:
         if (sq->write_register(reg, data) && index == 0) {
             square1.sweep_freq = square1.frequency();
-            if ((regs [0] & sq->period_mask) && (regs [0] & sq->shift_mask))
-            {
-                square1.sweep_delay = 1; // cause sweep to recalculate now
+            if ((regs[0] & sq->period_mask) && (regs[0] & sq->shift_mask)) {
+                square1.sweep_delay = 1;  // cause sweep to recalculate now
                 square1.clock_sweep();
             }
         }

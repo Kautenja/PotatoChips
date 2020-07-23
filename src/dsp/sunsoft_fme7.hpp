ba7903d1e869fb0508b9800f254f170422e1838c
Kautenja <ckfour@icloud.com>
2020-07-23 01:12:24 -0500

    cleanup

--- src/dsp/sunsoft_fme7.hpp
+++ src/dsp/sunsoft_fme7.hpp
@@ -58,10 +58,89 @@ class SunSoftFME7 {
 
     /// the registers on the chip
     uint8_t regs[REG_COUNT];
+
+    /// the oscillators on the chip
+    struct {
+        /// the output buffer to write samples to
+        BLIPBuffer* output;
+        /// the last amplitude value to output from the oscillator
+        int last_amp;
+    } oscs[OSC_COUNT];
     /// the value of the pulse waveform generators
-    bool phases[3] = {false, false, false};
+    bool phases[OSC_COUNT] = {false, false, false};
     /// delays for the oscillators
-    uint16_t delays[3] = {0, 0, 0};
+    uint16_t delays[OSC_COUNT] = {0, 0, 0};
+
+    /// the last time the oscillators were updated
+    blip_time_t last_time;
+
+    /// the synthesizer for generating sound from the chip
+    BLIPSynth<blip_good_quality, 1> synth;
+
+    /// Run the oscillators until the given end time.
+    ///
+    /// @param end_time the time to run the oscillators until
+    ///
+    void run_until(blip_time_t end_time) {
+        // assert(end_time >= last_time);
+        for (int index = 0; index < OSC_COUNT; index++) {
+            // int mode = regs[7] >> index;
+            int vol_mode = regs[010 + index];
+            int volume = AMP_TABLE[vol_mode & 0x0F];
+
+            BLIPBuffer* const osc_output = oscs[index].output;
+            if (!osc_output) continue;
+
+            // period
+            int const period_factor = 16;
+            unsigned period = (regs[index * 2 + 1] & 0x0F) * 0x100 * period_factor + regs[index * 2] * period_factor;
+            if (period < 50) {  // around 22 kHz
+                volume = 0;
+                if (!period) // on my AY-3-8910A, period doesn't have extra one added
+                    period = period_factor;
+            }
+
+            // current amplitude
+            int amp = volume;
+            if (!phases[index])
+                amp = 0;
+
+            {  // scope for amp change update
+                int delta = amp - oscs[index].last_amp;
+                if (delta) {
+                    oscs[index].last_amp = amp;
+                    synth.offset(last_time, delta, osc_output);
+                }
+            }
+
+            blip_time_t time = last_time + delays[index];
+            if (time < end_time) {
+                int delta = amp * 2 - volume;
+                if (volume) {
+                    do {
+                        delta = -delta;
+                        synth.offset(time, delta, osc_output);
+                        time += period;
+                    } while (time < end_time);
+                    oscs[index].last_amp = (delta + volume) >> 1;
+                    phases[index] = (delta > 0);
+                } else {
+                    // maintain phase when silent
+                    int count = (end_time - time + period - 1) / period;
+                    phases[index] ^= count & 1;
+                    time += (long) count * period;
+                }
+            }
+            delays[index] = time - end_time;
+        }
+        last_time = end_time;
+    }
+
+    /// Disable the copy constructor.
+    SunSoftFME7(const SunSoftFME7&);
+
+    /// Disable the assignment operator.
+    SunSoftFME7& operator=(const SunSoftFME7&);
 
  public:
     /// Initialize a new SunSoft FME7 chip emulator.
@@ -134,86 +213,6 @@ class SunSoftFME7 {
         if (time > last_time) run_until(time);
         assert(last_time >= time);
         last_time -= time;
-    }
-
- private:
-    /// Disable the copy constructor
-    SunSoftFME7(const SunSoftFME7&);
-
-    /// Disable the assignment operator
-    SunSoftFME7& operator=(const SunSoftFME7&);
-
-    /// the oscillators on the chip
-    struct {
-        /// the output buffer to write samples to
-        BLIPBuffer* output;
-        /// the last amplitude value to output from the oscillator
-        int last_amp;
-    } oscs[OSC_COUNT];
-
-    /// the last time the oscillators were updated
-    blip_time_t last_time;
-
-    /// the synthesizer for generating sound from the chip
-    BLIPSynth<blip_good_quality, 1> synth;
-
-    /// Run the oscillators until the given end time.
-    ///
-    /// @param end_time the time to run the oscillators until
-    ///
-    void run_until(blip_time_t end_time) {
-        // assert(end_time >= last_time);
-        for (int index = 0; index < OSC_COUNT; index++) {
-            // int mode = regs[7] >> index;
-            int vol_mode = regs[010 + index];
-            int volume = AMP_TABLE[vol_mode & 0x0F];
-
-            BLIPBuffer* const osc_output = oscs[index].output;
-            if (!osc_output) continue;
-
-            // period
-            int const period_factor = 16;
-            unsigned period = (regs[index * 2 + 1] & 0x0F) * 0x100 * period_factor + regs[index * 2] * period_factor;
-            if (period < 50) {  // around 22 kHz
-                volume = 0;
-                if (!period) // on my AY-3-8910A, period doesn't have extra one added
-                    period = period_factor;
-            }
-
-            // current amplitude
-            int amp = volume;
-            if (!phases[index])
-                amp = 0;
-
-            {  // scope for amp change update
-                int delta = amp - oscs[index].last_amp;
-                if (delta) {
-                    oscs[index].last_amp = amp;
-                    synth.offset(last_time, delta, osc_output);
-                }
-            }
-
-            blip_time_t time = last_time + delays[index];
-            if (time < end_time) {
-                int delta = amp * 2 - volume;
-                if (volume) {
-                    do {
-                        delta = -delta;
-                        synth.offset(time, delta, osc_output);
-                        time += period;
-                    } while (time < end_time);
-                    oscs[index].last_amp = (delta + volume) >> 1;
-                    phases[index] = (delta > 0);
-                } else {
-                    // maintain phase when silent
-                    int count = (end_time - time + period - 1) / period;
-                    phases[index] ^= count & 1;
-                    time += (long) count * period;
-                }
-            }
-            delays[index] = time - end_time;
-        }
-        last_time = end_time;
     }
 };
 

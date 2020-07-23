86661225b9c4ae7fc3c6b88c66ea938799f43aff
Kautenja <ckfour@icloud.com>
2020-07-23 16:16:33 -0500

    exception base classes

---
+++ src/dsp/exceptions.hpp
@@ -0,0 +1,88 @@
+// Exceptions that can be thrown by the application.
+// Copyright 2020 Christian Kauten
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef DSP_EXCEPTIONS_HPP_
+#define DSP_EXCEPTIONS_HPP_
+
+#include <exception>
+#include <string>
+
+/// An exception class.
+class Exception: public std::exception {
+ protected:
+    /// the error message.
+    std::string message;
+
+ public:
+    /// @brief Constructor (C strings).
+    /// @param message_ C-style string error message. The string contents are
+    /// copied upon construction. Hence, responsibility for deleting the char*
+    /// lies with the caller.
+    ///
+    explicit Exception(const char* message_) : message(message_) { }
+
+    /// @brief Constructor (C++ STL strings).
+    /// @param message_ The error message.
+    ///
+    explicit Exception(const std::string& message_) : message(message_) { }
+
+    /// @brief Destroy this exception.
+    ///
+    virtual ~Exception() throw () { }
+
+    /// @brief Returns a pointer to the (constant) error description.
+    /// @returns A pointer to a const char*. The underlying memory is in
+    /// possession of the Exception object. Callers must not attempt to free
+    /// the memory.
+    ///
+    virtual const char* what() const throw () { return message.c_str(); }
+};
+
+
+/// An exception class.
+class ChannelOutOfBoundsException: public std::exception {
+ protected:
+    /// the error message.
+    const std::string message;
+
+ public:
+    /// @brief Constructor (C strings).
+    /// @param channel the channel that was requested
+    /// @param num_channels the number of channels that are available
+    ///
+    explicit ChannelOutOfBoundsException(
+        unsigned channel,
+        unsigned num_channels
+    ) : message(
+        "tried to set output for channel index " +
+        std::to_string(channel) +
+        ", but the chip has " +
+        std::to_string(num_channels) +
+        " channels"
+    ) { }
+
+    /// @brief Destroy this exception.
+    ///
+    virtual ~ChannelOutOfBoundsException() throw () { }
+
+    /// @brief Returns a pointer to the (constant) error description.
+    /// @returns A pointer to a const char*. The underlying memory is in
+    /// possession of the Exception object. Callers must not attempt to free
+    /// the memory.
+    ///
+    virtual const char* what() const throw () { return message.c_str(); }
+};
+
+#endif  // DSP_EXCEPTIONS_HPP_

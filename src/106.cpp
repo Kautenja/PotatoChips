0bce763282b1a1f450f56c83e60813a14337b001
Kautenja <ckfour@icloud.com>
2020-08-09 13:03:08 -0500

    refactor

--- src/106.cpp
+++ src/106.cpp
@@ -27,12 +27,28 @@
 
 /// A Namco 106 Chip module.
 struct Chip106 : Module {
+ private:
+    /// The BLIP buffer to render audio samples from
+    BLIPBuffer buffers[Namco106::OSC_COUNT];
+    /// The 106 instance to synthesize sound with
+    Namco106 apu;
+    /// the number of active oscillators on the chip
+    unsigned num_oscillators = 1;
+
+    /// a clock divider for running CV acquisition slower than audio rate
+    dsp::ClockDivider cvDivider;
+    /// a clock divider for running LED updates slower than audio rate
+    dsp::ClockDivider lightsDivider;
+
+ public:
     /// the indexes of parameters (knobs, switches, etc.) on the module
     enum ParamIds {
         ENUMS(PARAM_FREQ, Namco106::OSC_COUNT),
         ENUMS(PARAM_VOLUME, Namco106::OSC_COUNT),
-        PARAM_NUM_CHANNELS, PARAM_NUM_CHANNELS_ATT,
-        PARAM_WAVETABLE, PARAM_WAVETABLE_ATT,
+        PARAM_NUM_OSCILLATORS,
+        PARAM_NUM_OSCILLATORS_ATT,
+        PARAM_WAVETABLE,
+        PARAM_WAVETABLE_ATT,
         PARAM_COUNT
     };
     /// the indexes of input ports on the module
@@ -40,7 +56,7 @@ struct Chip106 : Module {
         ENUMS(INPUT_VOCT, Namco106::OSC_COUNT),
         ENUMS(INPUT_FM, Namco106::OSC_COUNT),
         ENUMS(INPUT_VOLUME, Namco106::OSC_COUNT),
-        INPUT_NUM_CHANNELS,
+        INPUT_NUM_OSCILLATORS,
         INPUT_WAVETABLE,
         INPUT_COUNT
     };
@@ -55,62 +71,30 @@ struct Chip106 : Module {
         LIGHT_COUNT
     };
 
-    /// The BLIP buffer to render audio samples from
-    BLIPBuffer buf[Namco106::OSC_COUNT];
-    /// The 106 instance to synthesize sound with
-    Namco106 apu;
-    /// the number of active oscillators on the chip
-    unsigned num_oscillators = 1;
-
-    /// a clock divider for running CV acquisition slower than audio rate
-    dsp::ClockDivider cvDivider;
-    /// a clock divider for running LED updates slower than audio rate
-    dsp::ClockDivider lightsDivider;
-
     /// the bit-depth of the wave-table
     static constexpr auto BIT_DEPTH = 15;
     /// the number of samples in the wave-table
     static constexpr auto SAMPLES_PER_WAVETABLE = 32;
-
     /// the number of editors on the module
-    static constexpr int NUM_WAVETABLES = 5;
-
-    /// the samples in the wave-table (1)
-    uint8_t values0[SAMPLES_PER_WAVETABLE];
-    /// the samples in the wave-table (2)
-    uint8_t values1[SAMPLES_PER_WAVETABLE];
-    /// the samples in the wave-table (3)
-    uint8_t values2[SAMPLES_PER_WAVETABLE];
-    /// the samples in the wave-table (4)
-    uint8_t values3[SAMPLES_PER_WAVETABLE];
-    /// the samples in the wave-table (5)
-    uint8_t values4[SAMPLES_PER_WAVETABLE];
+    static constexpr int NUM_WAVEFORMS = 5;
 
     /// the wave-tables to morph between
-    uint8_t* values[NUM_WAVETABLES] = {
-        values0,
-        values1,
-        values2,
-        values3,
-        values4
-    };
+    uint8_t wavetable[NUM_WAVEFORMS][SAMPLES_PER_WAVETABLE];
 
     /// @brief Initialize a new 106 Chip module.
     Chip106() {
         config(PARAM_COUNT, INPUT_COUNT, OUTPUT_COUNT, LIGHT_COUNT);
-        configParam(PARAM_NUM_CHANNELS, 1, 8, 4, "Active Channels");
-        configParam(PARAM_NUM_CHANNELS_ATT, -1, 1, 0, "Active Channels Attenuverter");
-        configParam(PARAM_WAVETABLE, 1, 5, 1, "Wavetable Morph");
-        configParam(PARAM_WAVETABLE_ATT, -1, 1, 0, "Wavetable Morph Attenuverter");
+        configParam(PARAM_NUM_OSCILLATORS,      1, Namco106::OSC_COUNT, 4, "Active Channels");
+        configParam(PARAM_NUM_OSCILLATORS_ATT, -1, 1,                   0, "Active Channels Attenuverter");
+        configParam(PARAM_WAVETABLE,            1, NUM_WAVEFORMS,       1, "Waveform Morph");
+        configParam(PARAM_WAVETABLE_ATT,       -1, 1,                   0, "Waveform Morph Attenuverter");
         cvDivider.setDivision(16);
         lightsDivider.setDivision(128);
         // set the output buffer for each individual voice
-        for (unsigned i = 0; i < Namco106::OSC_COUNT; i++) {
-            auto descFreq = "Channel " + std::to_string(i + 1) + " Frequency";
-            configParam(PARAM_FREQ + i, -30.f, 30.f, 0.f, descFreq,  " Hz", dsp::FREQ_SEMITONE, dsp::FREQ_C4);
-            auto descVol = "Channel " + std::to_string(i + 1) + " Volume";
-            configParam(PARAM_VOLUME + i, 0, 15, 15, descVol,  "%", 0, 100.f / 15.f);
-            apu.set_output(i, &buf[i]);
+        for (unsigned oscillator = 0; oscillator < Namco106::OSC_COUNT; oscillator++) {
+            configParam(PARAM_FREQ + oscillator, -30.f, 30.f, 0.f, "Channel " + std::to_string(oscillator + 1) + " Frequency",  " Hz", dsp::FREQ_SEMITONE, dsp::FREQ_C4);
+            configParam(PARAM_VOLUME + oscillator, 0,   15,  15,   "Channel " + std::to_string(oscillator + 1) + " Volume",     "%",   0,                  100.f / 15.f);
+            apu.set_output(oscillator, &buffers[oscillator]);
         }
         // volume of 3 produces a roughly 5Vpp signal from all voices
         apu.set_volume(3.f);
@@ -122,34 +106,34 @@ struct Chip106 : Module {
 
     /// @brief Respond to the change of sample rate in the engine.
     inline void onSampleRateChange() override {
-        for (unsigned i = 0; i < Namco106::OSC_COUNT; i++)
-            buf[i].set_sample_rate(APP->engine->getSampleRate(), CLOCK_RATE);
+        for (unsigned oscillator = 0; oscillator < Namco106::OSC_COUNT; oscillator++)
+            buffers[oscillator].set_sample_rate(APP->engine->getSampleRate(), CLOCK_RATE);
     }
 
     /// @brief Respond to the user resetting the module with the "Initialize" action.
     void onReset() override {
         /// the default wave-table for each page of the wave-table editor
-        static constexpr uint8_t* wavetables[NUM_WAVETABLES] = {
+        static constexpr uint8_t* WAVETABLE[NUM_WAVEFORMS] = {
             SINE,
             PW5,
             RAMP_UP,
             TRIANGLE_DIST,
             RAMP_DOWN
         };
-        for (unsigned i = 0; i < NUM_WAVETABLES; i++)
-            memcpy(values[i], wavetables[i], SAMPLES_PER_WAVETABLE);
+        for (unsigned i = 0; i < NUM_WAVEFORMS; i++)
+            memcpy(wavetable[i], WAVETABLE[i], SAMPLES_PER_WAVETABLE);
     }
 
     /// @brief Respond to the user randomizing the module with the "Randomize" action.
     void onRandomize() override {
-        for (unsigned table = 0; table < NUM_WAVETABLES; table++) {
+        for (unsigned table = 0; table < NUM_WAVEFORMS; table++) {
             for (unsigned sample = 0; sample < SAMPLES_PER_WAVETABLE; sample++) {
-                values[table][sample] = random::u32() % BIT_DEPTH;
+                wavetable[table][sample] = random::u32() % BIT_DEPTH;
                 // interpolate between random samples to smooth slightly
                 if (sample > 0) {
-                    auto last = values[table][sample - 1];
-                    auto next = values[table][sample];
-                    values[table][sample] = (last + next) / 2;
+                    auto last = wavetable[table][sample - 1];
+                    auto next = wavetable[table][sample];
+                    wavetable[table][sample] = (last + next) / 2;
                 }
             }
         }
@@ -158,11 +142,11 @@ struct Chip106 : Module {
     /// @brief Convert the module's state to a JSON object.
     json_t* dataToJson() override {
         json_t* rootJ = json_object();
-        for (int table = 0; table < NUM_WAVETABLES; table++) {
+        for (int table = 0; table < NUM_WAVEFORMS; table++) {
             json_t* array = json_array();
             for (int sample = 0; sample < SAMPLES_PER_WAVETABLE; sample++)
-                json_array_append_new(array, json_integer(values[table][sample]));
-            auto key = "values" + std::to_string(table);
+                json_array_append_new(array, json_integer(wavetable[table][sample]));
+            auto key = "wavetable" + std::to_string(table);
             json_object_set_new(rootJ, key.c_str(), array);
         }
 
@@ -171,34 +155,34 @@ struct Chip106 : Module {
 
     /// @brief Load the module's state from a JSON object.
     void dataFromJson(json_t* rootJ) override {
-        for (int table = 0; table < NUM_WAVETABLES; table++) {
-            auto key = "values" + std::to_string(table);
+        for (int table = 0; table < NUM_WAVEFORMS; table++) {
+            auto key = "wavetable" + std::to_string(table);
             json_t* data = json_object_get(rootJ, key.c_str());
             if (data) {
                 for (int sample = 0; sample < SAMPLES_PER_WAVETABLE; sample++)
-                    values[table][sample] = json_integer_value(json_array_get(data, sample));
+                    wavetable[table][sample] = json_integer_value(json_array_get(data, sample));
             }
         }
     }
 
     /// @brief Return the active oscillators parameter.
     ///
-    /// @returns the active oscillator count in [1, 8]
+    /// @returns the active oscillator count \f$\in [1, 8]\f$
     ///
     inline uint8_t getActiveOscillators() {
-        auto param = params[PARAM_NUM_CHANNELS].getValue();
-        auto att = params[PARAM_NUM_CHANNELS_ATT].getValue();
+        auto param = params[PARAM_NUM_OSCILLATORS].getValue();
+        auto att = params[PARAM_NUM_OSCILLATORS_ATT].getValue();
         // get the CV as 1V per oscillator
-        auto cv = 8.f * inputs[INPUT_NUM_CHANNELS].getVoltage() / 10.f;
+        auto cv = 8.f * inputs[INPUT_NUM_OSCILLATORS].getVoltage() / 10.f;
         // oscillators are indexed maths style on the chip, not CS style
         return rack::math::clamp(param + att * cv, 1.f, 8.f);
     }
 
-    /// @brief Return the wave-table parameter.
+    /// @brief Return the wave-table position parameter.
     ///
-    /// @returns the floating index of the wave-table table in [0, 4]
+    /// @returns the floating index of the wave-table position \f$\in [0, 4]\f$
     ///
-    inline float getWavetable() {
+    inline float getWavetablePosition() {
         auto param = params[PARAM_WAVETABLE].getValue();
         auto att = params[PARAM_WAVETABLE_ATT].getValue();
         // get the CV as 1V per wave-table
@@ -224,7 +208,7 @@ struct Chip106 : Module {
         static constexpr uint32_t wave_length = 64 - (SAMPLES_PER_WAVETABLE / 4);
         // ignoring num_oscillators in the calculation allows the standard 103
         // function where additional oscillators reduce the frequency of all
-        freq *= (wave_length * 15.f * 65536.f) / buf[0].get_clock_rate();
+        freq *= (wave_length * 15.f * 65536.f) / buffers[oscillator].get_clock_rate();
         // clamp within the legal bounds for the frequency value
         freq = rack::clamp(freq, 4.f, 262143.f);
         // OR the waveform length into the high 6 bits of "frequency Hi"
@@ -266,27 +250,28 @@ struct Chip106 : Module {
         // the amount of voltage per increment of 16-bit fidelity volume
         static constexpr float divisor = std::numeric_limits<int16_t>::max();
         // convert the 16-bit sample to 10Vpp floating point
-        return Vpp * buf[oscillator].read_sample() / divisor;
+        return Vpp * buffers[oscillator].read_sample() / divisor;
     }
 
     /// @brief Process a sample.
     void process(const ProcessArgs &args) override {
         if (cvDivider.process()) {
-            // write the waveform data to the chip's RAM
-            auto wavetable = getWavetable();
+            // write waveform data to the chip's RAM based on the position in
+            // the wave-table
+            auto position = getWavetablePosition();
             // calculate the address of the base waveform in the table
-            int wavetable0 = floor(wavetable);
+            int wavetable0 = floor(position);
             // calculate the address of the next waveform in the table
-            int wavetable1 = ceil(wavetable);
+            int wavetable1 = ceil(position);
             // calculate floating point offset between the base and next table
-            float interpolate = wavetable - wavetable0;
+            float interpolate = position - wavetable0;
             for (int i = 0; i < SAMPLES_PER_WAVETABLE / 2; i++) {  // iterate over nibbles
                 // get the first waveform data
-                auto nibbleHi0 = values[wavetable0][2 * i];
-                auto nibbleLo0 = values[wavetable0][2 * i + 1];
+                auto nibbleHi0 = wavetable[wavetable0][2 * i];
+                auto nibbleLo0 = wavetable[wavetable0][2 * i + 1];
                 // get the second waveform data
-                auto nibbleHi1 = values[wavetable1][2 * i];
-                auto nibbleLo1 = values[wavetable1][2 * i + 1];
+                auto nibbleHi1 = wavetable[wavetable1][2 * i];
+                auto nibbleLo1 = wavetable[wavetable1][2 * i + 1];
                 // floating point interpolation
                 uint8_t nibbleHi = ((1.f - interpolate) * nibbleHi0 + interpolate * nibbleHi1);
                 uint8_t nibbleLo = ((1.f - interpolate) * nibbleLo0 + interpolate * nibbleLo1);
@@ -317,13 +302,13 @@ struct Chip106 : Module {
         }
         // process audio samples on the chip engine
         apu.end_frame(CLOCK_RATE / args.sampleRate);
-        for (unsigned i = 0; i < Namco106::OSC_COUNT; i++)
-            outputs[i].setVoltage(getAudioOut(i));
+        for (unsigned oscillator = 0; oscillator < Namco106::OSC_COUNT; oscillator++)
+            outputs[oscillator].setVoltage(getAudioOut(oscillator));
         // set the VU meter lights if the light divider is high
         if (lightsDivider.process()) {
-            for (unsigned i = 0; i < Namco106::OSC_COUNT; i++) {
-                auto light = LIGHT_CHANNEL + Namco106::OSC_COUNT - i - 1;
-                lights[light].setSmoothBrightness(i < num_oscillators, args.sampleTime * lightsDivider.getDivision());
+            for (unsigned oscillator = 0; oscillator < Namco106::OSC_COUNT; oscillator++) {
+                auto light = LIGHT_CHANNEL + Namco106::OSC_COUNT - oscillator - 1;
+                lights[light].setSmoothBrightness(oscillator < num_oscillators, args.sampleTime * lightsDivider.getDivision());
             }
         }
     }
@@ -348,11 +333,8 @@ struct Chip106Widget : ModuleWidget {
         addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
         addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-        // if the module is displaying in/being rendered for the library, the
-        // module will be null and a dummy waveform is displayed
-        auto module_ = reinterpret_cast<Chip106*>(this->module);
         // the fill colors for the wave-table editor lines
-        static constexpr NVGcolor colors[Chip106::NUM_WAVETABLES] = {
+        static constexpr NVGcolor colors[Chip106::NUM_WAVEFORMS] = {
             {{{1.f, 0.f, 0.f, 1.f}}},  // red
             {{{0.f, 1.f, 0.f, 1.f}}},  // green
             {{{0.f, 0.f, 1.f, 1.f}}},  // blue
@@ -360,34 +342,37 @@ struct Chip106Widget : ModuleWidget {
             {{{1.f, 1.f, 1.f, 1.f}}}   // white
         };
         /// the default wave-table for each page of the wave-table editor
-        static constexpr uint8_t* wavetables[Chip106::NUM_WAVETABLES] = {
+        static constexpr uint8_t* wavetables[Chip106::NUM_WAVEFORMS] = {
             SINE,
             PW5,
             RAMP_UP,
             TRIANGLE_DIST,
             RAMP_DOWN
         };
-        // add wave-table editors
-        for (int i = 0; i < Chip106::NUM_WAVETABLES; i++) {
+        // Add wave-table editors. If the module is displaying in/being
+        // rendered for the library, the module will be null and a dummy
+        // waveform is displayed
+        for (int waveform = 0; waveform < Chip106::NUM_WAVEFORMS; waveform++) {
             // get the wave-table buffer for this editor
-            uint8_t* wavetable =
-                module ? &module_->values[i][0] : &wavetables[i][0];
+            uint8_t* wavetable = module ?
+                    &reinterpret_cast<Chip106*>(this->module)->wavetable[waveform][0] :
+                    &wavetables[waveform][0];
             // setup a table editor for the buffer
             auto table_editor = new WaveTableEditor<uint8_t>(
                 wavetable,                       // wave-table buffer
                 Chip106::SAMPLES_PER_WAVETABLE,  // wave-table length
                 Chip106::BIT_DEPTH,              // waveform bit depth
-                Vec(10, 26 + 67 * i),            // position
+                Vec(10, 26 + 67 * waveform),     // position
                 Vec(135, 60),                    // size
-                colors[i]                        // line fill color
+                colors[waveform]                 // line fill color
             );
             // add the table editor to the module
             addChild(table_editor);
         }
         // oscillator select
-        addParam(createParam<Rogan3PSNES>(Vec(155, 38), module, Chip106::PARAM_NUM_CHANNELS));
-        addParam(createParam<Rogan1PSNES>(Vec(161, 88), module, Chip106::PARAM_NUM_CHANNELS_ATT));
-        addInput(createInput<PJ301MPort>(Vec(164, 126), module, Chip106::INPUT_NUM_CHANNELS));
+        addParam(createParam<Rogan3PSNES>(Vec(155, 38), module, Chip106::PARAM_NUM_OSCILLATORS));
+        addParam(createParam<Rogan1PSNES>(Vec(161, 88), module, Chip106::PARAM_NUM_OSCILLATORS_ATT));
+        addInput(createInput<PJ301MPort>(Vec(164, 126), module, Chip106::INPUT_NUM_OSCILLATORS));
         // wave-table morph
         addParam(createParam<Rogan3PSNES>(Vec(155, 183), module, Chip106::PARAM_WAVETABLE));
         addParam(createParam<Rogan1PSNES>(Vec(161, 233), module, Chip106::PARAM_WAVETABLE_ATT));

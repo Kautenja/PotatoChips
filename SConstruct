ded1c81a84342600edd6b5ae9cbde07a3098f4f7
Kautenja <kautencreations@gmail.com>
2020-09-29 17:25:40 -0500

    SConstruct for building test code

---
+++ SConstruct
@@ -0,0 +1,146 @@
+"""The compilation script for this project using SCons."""
+import os
+import fnmatch
+import re
+
+
+# create a separate build directory
+VariantDir('build_src', 'src/dsp', duplicate=0)
+VariantDir('build_benchmark', 'benchmark', duplicate=0)
+VariantDir('build_test', 'test', duplicate=0)
+
+
+# the compiler and linker flags for the production C++ environment
+PROD_FLAGS = [
+    '-std=c++17',
+    '-pthread',
+    '-O3',
+    '-march=native',
+    '-pipe',
+    '-pedantic',
+    '-Wall'
+]
+
+
+# include for the production environment
+INCLUDES = [
+    '#src',
+]
+
+
+# the compiler and linker flags for the testing C++ environment
+TEST_FLAGS = [
+    '-std=c++17',
+    '-pthread',
+    '-march=native',
+    '-pipe',
+    '-pedantic',
+    '-Wall'
+]
+
+
+# include for the testing and benchmarking environment
+TEST_INCLUDES = [
+    '#dep/Catch2/single_include/catch2',
+]
+
+
+TESTING_ENV = Environment(
+    ENV=os.environ,
+    CXX='g++',
+    CPPFLAGS=['-Wno-unused-value'],
+    CXXFLAGS=TEST_FLAGS,
+    LINKFLAGS=TEST_FLAGS,
+    CPPPATH=INCLUDES + TEST_INCLUDES,
+)
+
+
+PRODUCTION_ENV = Environment(
+    ENV=os.environ,
+    CXX='g++',
+    CPPFLAGS=['-Wno-unused-value'],
+    CXXFLAGS=PROD_FLAGS,
+    LINKFLAGS=PROD_FLAGS,
+    CPPPATH=INCLUDES,
+)
+
+
+BENCHMARK_ENV = Environment(
+    ENV=os.environ,
+    CXX='g++',
+    CPPFLAGS=['-Wno-unused-value'],
+    CXXFLAGS=PROD_FLAGS,
+    LINKFLAGS=PROD_FLAGS,
+    CPPPATH=INCLUDES + TEST_INCLUDES,
+)
+
+
+def find_source_files(src_dir, build_dir):
+    """
+    Find all the source files in the given directory.
+
+    Args:
+        src_dir: the source directory to search through
+        build_dir: the build directory (to replace src_dir with)
+
+    Returns:
+        a list of paths to cpp files where src_dir is replaced by build_dir
+    """
+    files = []
+    for root, dirnames, filenames in os.walk(src_dir):
+        root = re.sub(src_dir, build_dir, root)
+        for filename in fnmatch.filter(filenames, '*.cpp'):
+            files.append(os.path.join(root, filename))
+    return sorted(files)
+
+
+# Locate all the C++ source files
+SRC = find_source_files('src/dsp', 'build_src/')
+# create separate object files for testing and production environments
+TEST_SRC = [TESTING_ENV.Object(f.replace('.cpp', '') + '-test', f) for f in SRC]
+PROD_SRC = [PRODUCTION_ENV.Object(f.replace('.cpp', '') + '-prod', f) for f in SRC]
+BENCHMARK_SRC = [BENCHMARK_ENV.Object(f.replace('.cpp', '') + '-bench', f) for f in SRC]
+
+
+#
+# MARK: Unit Tests
+#
+
+
+# locate all the testing source files
+TEST_FILES = find_source_files('test/', 'build_test/')
+# create a list to store all the test target aliases in
+UNIT_TEST_ALIASES = []
+for file in TEST_FILES:  # iterate over all the test source files
+    UNIT_TEST_PROGRAM = TESTING_ENV.Program(file.replace('.cpp', ''), [file] + TEST_SRC)
+    UNIT_TEST_ALIASES.append(Alias('test/' + file.replace('build_test/', ''), [UNIT_TEST_PROGRAM], UNIT_TEST_PROGRAM[0].path))
+    AlwaysBuild(UNIT_TEST_ALIASES[-1])
+
+
+# create an alias to run all test suites
+Alias("test", UNIT_TEST_ALIASES)
+
+
+#
+# MARK: Benchmarks
+#
+
+
+for benchmark in find_source_files('benchmark', 'build_benchmark'):
+    # create the binary
+    binary_name = benchmark.replace('.cpp', '')
+    program = BENCHMARK_ENV.Program(binary_name, [benchmark] + BENCHMARK_SRC)
+    # create a scons alias for the binary
+    shell_name = benchmark.replace('build_', '')
+    benchmark_alias = Alias(shell_name, [program], program[0].path)
+    AlwaysBuild(benchmark_alias)
+
+
+#
+# MARK: DSP Library
+#
+
+
+# Create a shared library (it will add "lib" to the front automatically)
+lib = PRODUCTION_ENV.SharedLibrary('_PotatoChips.so', SRC)
+AlwaysBuild(lib)
---
+++ test/dsp/test_sony_s_dsp_echo.cpp
@@ -0,0 +1,109 @@
+// Test cases for the Sony S-DSP Echo emulator.
+//
+// Copyright (c) 2020 Christian Kauten
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "dsp/sony_s_dsp_echo.hpp"
+
+TEST_CASE("Sony_S_DSP_Echo::BufferSample should be 4 bytes") {
+    REQUIRE(4 == sizeof(Sony_S_DSP_Echo::BufferSample));
+}
+
+TEST_CASE("Sony_S_DSP_Echo::BufferSample should have correct constants") {
+    REQUIRE(0 == Sony_S_DSP_Echo::BufferSample::LEFT);
+    REQUIRE(1 == Sony_S_DSP_Echo::BufferSample::RIGHT);
+    // REQUIRE(2 == Sony_S_DSP_Echo::BufferSample::CHANNELS);
+}
+
+// ---------------------------------------------------------------------------
+// MARK: Order
+// ---------------------------------------------------------------------------
+
+// TEST_CASE("initialize default Order") {
+//     Order order;
+//     REQUIRE(order.next == nullptr);
+//     REQUIRE(order.prev == nullptr);
+//     REQUIRE(order.uid == 0);
+//     REQUIRE(order.side == Side::Sell);
+//     REQUIRE(order.quantity == 0);
+//     REQUIRE(order.price == 0);
+//     REQUIRE(order.limit == nullptr);
+// }
+
+// SCENARIO("initialize Order") {
+//     GIVEN("arbitrary legal parameters") {
+//         UID uid = 5;
+//         auto side = Side::Buy;
+//         Quantity quantity = 100;
+//         Price price = 5746;
+//         WHEN("an Order is initialized") {
+//             Order order = {uid, side, quantity, price};
+//             THEN("the order is created with parameters") {
+//                 REQUIRE(order.next == nullptr);
+//                 REQUIRE(order.prev == nullptr);
+//                 REQUIRE(order.uid == uid);
+//                 REQUIRE(order.side == side);
+//                 REQUIRE(order.quantity == quantity);
+//                 REQUIRE(order.price == price);
+//                 REQUIRE(order.limit == nullptr);
+//             }
+//         }
+//     }
+// }
+
+// ---------------------------------------------------------------------------
+// MARK: Limit
+// ---------------------------------------------------------------------------
+
+// TEST_CASE("initialize default Limit") {
+//     Limit limit;
+//     REQUIRE(limit.key == 0);
+//     REQUIRE(limit.parent == nullptr);
+//     REQUIRE(limit.left == nullptr);
+//     REQUIRE(limit.right == nullptr);
+//     REQUIRE(limit.count == 0);
+//     REQUIRE(limit.volume == 0);
+//     REQUIRE(limit.order_head == nullptr);
+//     REQUIRE(limit.order_tail == nullptr);
+// }
+
+// SCENARIO("initialize Limit") {
+//     GIVEN("arbitrary legal parameters and a new order") {
+//         Quantity quantity = 100;
+//         Price price = 5;
+//         Order order = {5, Side::Buy, quantity, price};
+//         WHEN("a Limit is initialized") {
+//             Limit limit{&order};
+//             THEN("the limit is created with parameters") {
+//                 REQUIRE(limit.key == price);
+//                 REQUIRE(limit.parent == nullptr);
+//                 REQUIRE(limit.left == nullptr);
+//                 REQUIRE(limit.right == nullptr);
+//                 REQUIRE(limit.count == 1);
+//                 REQUIRE(limit.volume == quantity);
+//                 REQUIRE(limit.order_head == &order);
+//                 REQUIRE(limit.order_tail == &order);
+//             }
+//         }
+//     }
+// }

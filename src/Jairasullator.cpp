5d225a030d90445d17f54afca3570357b03f21f5
Kautenja <kautencreations@gmail.com>
2020-10-30 11:36:37 -0500

    TODO

--- src/Jairasullator.cpp
+++ src/Jairasullator.cpp
@@ -17,6 +17,7 @@
 #include "engine/chip_module.hpp"
 #include "dsp/general_instrument_ay_3_8910.hpp"
 
+// TODO: draw normals on panel
 // TODO: setup INPUT_ENVELOPE
 // TODO: context menu for switching between envelope modes
 // TODO: document both modes off (4-bit dac based on amp port!)

9ef8de3ca2ee98e6cc1b0a296fb3975c29d95b3f
Kautenja <ckfour@icloud.com>
2020-11-08 18:26:27 -0600

    CV lights

--- src/S_SMP_ADSR.cpp
+++ src/S_SMP_ADSR.cpp
@@ -17,11 +17,6 @@
 #include "plugin.hpp"
 #include "dsp/sony_s_dsp/adsr.hpp"
 
-// TODO: lights
-// - on sliders for CV levels
-// - on panel for stage indication
-// TODO: EOR / EOC outputs
-
 // ---------------------------------------------------------------------------
 // MARK: Module
 // ---------------------------------------------------------------------------
@@ -38,6 +33,8 @@ struct ChipS_SMP_ADSR : Module {
     rack::dsp::BooleanTrigger gateTrigger[LANES][PORT_MAX_CHANNELS];
     /// triggers for handling input re-trigger signals
     rack::dsp::BooleanTrigger retrigTrigger[LANES][PORT_MAX_CHANNELS];
+    /// a clock divider for light updates
+    rack::dsp::ClockDivider lightDivider;
 
  public:
     /// the indexes of parameters (knobs, switches, etc.) on the module
@@ -69,7 +66,14 @@ struct ChipS_SMP_ADSR : Module {
     };
 
     /// the indexes of lights on the module
-    enum LightIds { NUM_LIGHTS };
+    enum LightIds {
+        ENUMS(LIGHT_AMPLITUDE,     3 * LANES),
+        ENUMS(LIGHT_ATTACK,        3 * LANES),
+        ENUMS(LIGHT_DECAY,         3 * LANES),
+        ENUMS(LIGHT_SUSTAIN_LEVEL, 3 * LANES),
+        ENUMS(LIGHT_SUSTAIN_RATE,  3 * LANES),
+        NUM_LIGHTS
+    };
 
     /// @brief Initialize a new S-DSP Chip module.
     ChipS_SMP_ADSR() {
@@ -81,18 +85,32 @@ struct ChipS_SMP_ADSR : Module {
             configParam(PARAM_SUSTAIN_LEVEL + lane,    0,   7,   5, "Sustain Level", "%", 0, 100.f / 7.f);
             configParam(PARAM_SUSTAIN_RATE  + lane,    0,  31,  20, "Sustain Rate");
         }
+        lightDivider.setDivision(512);
     }
 
  protected:
+    void set_light(unsigned light, unsigned lane, float cv, float sample_time) {
+        if (cv > 0) {
+            lights[light + 3 * lane + 0].setSmoothBrightness(0, sample_time);
+            lights[light + 3 * lane + 1].setSmoothBrightness(cv / 10.f, sample_time);
+            lights[light + 3 * lane + 2].setSmoothBrightness(0, sample_time);
+        } else {
+            lights[light + 3 * lane + 0].setSmoothBrightness(-cv / 10.f, sample_time);
+            lights[light + 3 * lane + 1].setSmoothBrightness(0, sample_time);
+            lights[light + 3 * lane + 2].setSmoothBrightness(0, sample_time);
+        }
+    }
+
     /// @brief Return the value of the attack parameter from the panel.
     ///
     /// @param channel the polyphonic channel to get the attack parameter for
     /// @param lane the processing lane to get the attack rate parameter for
     /// @returns the 8-bit attack parameter after applying CV modulations
     ///
-    inline uint8_t getAttack(unsigned channel, unsigned lane) {
+    inline uint8_t getAttack(unsigned channel, unsigned lane, bool update_light = true) {
         const float param = params[PARAM_ATTACK + lane].getValue();
-        const float cv = inputs[INPUT_ATTACK + lane].getPolyVoltage(channel);
+        auto port = inputs[INPUT_ATTACK + lane];
+        const float cv = port.isMonophonic() ? port.getVoltageSum() : port.getVoltage(channel);
         const float mod = std::numeric_limits<int8_t>::max() * cv / 10.f;
         // invert attack so it increases in time as it increase in value
         return 15 - clamp(param + mod, 0.f, 15.f);
@@ -106,7 +124,8 @@ struct ChipS_SMP_ADSR : Module {
     ///
     inline uint8_t getDecay(unsigned channel, unsigned lane) {
         const float param = params[PARAM_DECAY + lane].getValue();
-        const float cv = inputs[INPUT_DECAY + lane].getPolyVoltage(channel);
+        auto port = inputs[INPUT_DECAY + lane];
+        const float cv = port.isMonophonic() ? port.getVoltageSum() : port.getVoltage(channel);
         const float mod = std::numeric_limits<int8_t>::max() * cv / 10.f;
         // invert decay so it increases in time as it increase in value
         return 7 - clamp(param + mod, 0.f, 7.f);
@@ -120,7 +139,8 @@ struct ChipS_SMP_ADSR : Module {
     ///
     inline uint8_t getSustainRate(unsigned channel, unsigned lane) {
         const float param = params[PARAM_SUSTAIN_RATE + lane].getValue();
-        const float cv = inputs[INPUT_SUSTAIN_RATE + lane].getPolyVoltage(channel);
+        auto port = inputs[INPUT_SUSTAIN_RATE + lane];
+        const float cv = port.isMonophonic() ? port.getVoltageSum() : port.getVoltage(channel);
         const float mod = std::numeric_limits<int8_t>::max() * cv / 10.f;
         // invert sustain rate so it increases in time as it increase in value
         return 31 - clamp(param + mod, 0.f, 31.f);
@@ -134,7 +154,8 @@ struct ChipS_SMP_ADSR : Module {
     ///
     inline uint8_t getSustainLevel(unsigned channel, unsigned lane) {
         const float param = params[PARAM_SUSTAIN_LEVEL + lane].getValue();
-        const float cv = inputs[INPUT_SUSTAIN_LEVEL + lane].getPolyVoltage(channel);
+        auto port = inputs[INPUT_SUSTAIN_LEVEL + lane];
+        const float cv = port.isMonophonic() ? port.getVoltageSum() : port.getVoltage(channel);
         const float mod = std::numeric_limits<int8_t>::max() * cv / 10.f;
         return clamp(param + mod, 0.f, 7.f);
     }
@@ -147,7 +168,8 @@ struct ChipS_SMP_ADSR : Module {
     ///
     inline int8_t getAmplitude(unsigned channel, unsigned lane) {
         const float param = params[PARAM_AMPLITUDE + lane].getValue();
-        const float cv = inputs[INPUT_AMPLITUDE + lane].getPolyVoltage(channel);
+        auto port = inputs[INPUT_AMPLITUDE + lane];
+        const float cv = port.isMonophonic() ? port.getVoltageSum() : port.getVoltage(channel);
         const float mod = std::numeric_limits<int8_t>::max() * cv / 10.f;
         static constexpr float MIN = std::numeric_limits<int8_t>::min();
         static constexpr float MAX = std::numeric_limits<int8_t>::max();
@@ -163,10 +185,10 @@ struct ChipS_SMP_ADSR : Module {
     ///
     inline bool getTrigger(unsigned channel, unsigned lane) {
         // get the trigger from the gate input
-        const auto gateCV = rescale(inputs[INPUT_GATE + lane].getPolyVoltage(channel), 0.f, 2.f, 0.f, 1.f);
+        const auto gateCV = rescale(inputs[INPUT_GATE + lane].getVoltage(channel), 0.f, 2.f, 0.f, 1.f);
         const bool gate = gateTrigger[lane][channel].process(gateCV);
         // get the trigger from the re-trigger input
-        const auto retrigCV = rescale(inputs[INPUT_RETRIG + lane].getPolyVoltage(channel), 0.f, 2.f, 0.f, 1.f);
+        const auto retrigCV = rescale(inputs[INPUT_RETRIG + lane].getVoltage(channel), 0.f, 2.f, 0.f, 1.f);
         const bool retrig = retrigTrigger[lane][channel].process(retrigCV);
         // OR the two boolean values together
         return gate || retrig;
@@ -211,6 +233,16 @@ struct ChipS_SMP_ADSR : Module {
             for (unsigned channel = 0; channel < channels; channel++)
                 processChannel(channel, lane);
         }
+        if (lightDivider.process()) {
+            const auto sample_time = lightDivider.getDivision() * args.sampleTime;
+            for (unsigned lane = 0; lane < LANES; lane++) {
+                set_light(LIGHT_AMPLITUDE, lane, inputs[INPUT_AMPLITUDE + lane].getVoltage(0), sample_time);
+                set_light(LIGHT_ATTACK, lane, inputs[INPUT_ATTACK + lane].getVoltage(0), sample_time);
+                set_light(LIGHT_DECAY, lane, inputs[INPUT_DECAY + lane].getVoltage(0), sample_time);
+                set_light(LIGHT_SUSTAIN_LEVEL, lane, inputs[INPUT_SUSTAIN_LEVEL + lane].getVoltage(0), sample_time);
+                set_light(LIGHT_SUSTAIN_RATE, lane, inputs[INPUT_SUSTAIN_RATE + lane].getVoltage(0), sample_time);
+            }
+        }
     }
 };
 
@@ -239,27 +271,27 @@ struct ChipS_SMP_ADSRWidget : ModuleWidget {
             addInput(createInput<PJ301MPort>(Vec(20, 100 + 169 * i), module, ChipS_SMP_ADSR::INPUT_RETRIG + i));
             addOutput(createOutput<PJ301MPort>(Vec(20, 156 + 169 * i), module, ChipS_SMP_ADSR::OUTPUT_ENVELOPE + i));
             // Amplitude
-            auto amplitude = createParam<BefacoSlidePot>(Vec(66, 22 + 169 * i), module, ChipS_SMP_ADSR::PARAM_AMPLITUDE + i);
+            auto amplitude = createLightParam<LEDLightSlider<RedGreenBlueLight>>(Vec(66, 40 + 169 * i), module, ChipS_SMP_ADSR::PARAM_AMPLITUDE + i, ChipS_SMP_ADSR::LIGHT_AMPLITUDE + 3 * i);
             amplitude->snap = true;
             addParam(amplitude);
             addInput(createInput<PJ301MPort>(Vec(61, 157 + 169 * i), module, ChipS_SMP_ADSR::INPUT_AMPLITUDE + i));
             // Attack
-            auto attack = createParam<BefacoSlidePot>(Vec(100, 22 + 169 * i), module, ChipS_SMP_ADSR::PARAM_ATTACK + i);
+            auto attack = createLightParam<LEDLightSlider<RedGreenBlueLight>>(Vec(100, 40 + 169 * i), module, ChipS_SMP_ADSR::PARAM_ATTACK + i, ChipS_SMP_ADSR::LIGHT_ATTACK + 3 * i);
             attack->snap = true;
             addParam(attack);
             addInput(createInput<PJ301MPort>(Vec(95, 157 + 169 * i), module, ChipS_SMP_ADSR::INPUT_ATTACK + i));
             // Decay
-            auto decay = createParam<BefacoSlidePot>(Vec(134, 22 + 169 * i), module, ChipS_SMP_ADSR::PARAM_DECAY + i);
+            auto decay = createLightParam<LEDLightSlider<RedGreenBlueLight>>(Vec(134, 40 + 169 * i), module, ChipS_SMP_ADSR::PARAM_DECAY + i, ChipS_SMP_ADSR::LIGHT_DECAY + 3 * i);
             decay->snap = true;
             addParam(decay);
             addInput(createInput<PJ301MPort>(Vec(129, 157 + 169 * i), module, ChipS_SMP_ADSR::INPUT_DECAY + i));
             // Sustain Level
-            auto sustainLevel = createParam<BefacoSlidePot>(Vec(168, 22 + 169 * i), module, ChipS_SMP_ADSR::PARAM_SUSTAIN_LEVEL + i);
+            auto sustainLevel = createLightParam<LEDLightSlider<RedGreenBlueLight>>(Vec(168, 40 + 169 * i), module, ChipS_SMP_ADSR::PARAM_SUSTAIN_LEVEL + i, ChipS_SMP_ADSR::LIGHT_SUSTAIN_LEVEL + 3 * i);
             sustainLevel->snap = true;
             addParam(sustainLevel);
             addInput(createInput<PJ301MPort>(Vec(163, 157 + 169 * i), module, ChipS_SMP_ADSR::INPUT_SUSTAIN_LEVEL + i));
             // Sustain Rate
-            auto sustainRate = createParam<BefacoSlidePot>(Vec(202, 22 + 169 * i), module, ChipS_SMP_ADSR::PARAM_SUSTAIN_RATE + i);
+            auto sustainRate = createLightParam<LEDLightSlider<RedGreenBlueLight>>(Vec(202, 40 + 169 * i), module, ChipS_SMP_ADSR::PARAM_SUSTAIN_RATE + i, ChipS_SMP_ADSR::LIGHT_SUSTAIN_RATE + 3 * i);
             sustainRate->snap = true;
             addParam(sustainRate);
             addInput(createInput<PJ301MPort>(Vec(197, 157 + 169 * i), module, ChipS_SMP_ADSR::INPUT_SUSTAIN_RATE + i));

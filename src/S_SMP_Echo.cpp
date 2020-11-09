5363343365dbce700112b6733cb6d79b7c164d0b
Kautenja <ckfour@icloud.com>
2020-11-09 15:30:09 -0600

    resolve TODO

--- src/S_SMP_Echo.cpp
+++ src/S_SMP_Echo.cpp
@@ -16,7 +16,6 @@
 #include "plugin.hpp"
 #include "dsp/sony_s_dsp/echo.hpp"
 
-// TODO: option to prevent clicks in context menu
 // TODO: VU meters
 // TODO: bypass switch / rethink the wet/dry
 // TODO: explosion indicator for FIR filter
